a27522addf9221137d4110c326f4e861b71f1fe6
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2025-05-16 14:25:04 +0530

    Modified location of examples folder

--- internal/examples/data-sources/nios_RecordsA/data-source.tf
+++
@@ -1,5 +0,0 @@
-data "nios_RecordsA" "read_record_via_extattrs" {
-  filters = {
-    "Site" = "Siteblr"
-  }
-}
--- internal/examples/provider/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "nios" {
-    nios_auth="username:password"
-    nios_host_url="<Nios Host URL>"
-    }
