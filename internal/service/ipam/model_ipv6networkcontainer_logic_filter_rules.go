87f0476516e3103d0bf95a0e190236693fd8cfcd
JchhatbarInfoblox <jchhatbar@infoblox.com>
2025-07-10 17:03:05 +0530

    Add models for IPv6 network container settings
    - Introduced `Ipv6networkcontainerPortControlBlackoutSettingModel` to manage blackout settings including enablement, duration, and schedule.
    - Created `Ipv6networkcontainerSubscribeSettingsModel` for handling subscription settings with enabled attributes and mapped extensible attributes.
    - Added `Ipv6networkcontainerZoneAssociationsModel` to represent zone associations with FQDN, default status, and view.
    - Implemented `Ipv6networkcontainercloudinfoDelegatedMemberModel` for cloud info delegated members with IPv4 and IPv6 addresses.
    - Developed `Ipv6networkcontainerdiscoverybasicpollsettingsSwitchPortDataCollectionPollingScheduleModel` for scheduling data collection with various time and frequency attributes.
    - Established `Ipv6networkcontainerdiscoveryblackoutsettingBlackoutScheduleModel` for blackout scheduling with detailed timing options.
    - Created `Ipv6networkcontainerportcontrolblackoutsettingBlackoutScheduleModel` for managing blackout schedules in port control settings.
    - Introduced `Ipv6networkcontainersubscribesettingsMappedEaAttributesModel` for mapping Cisco ISE attributes to extensible attributes.

---
+++ internal/service/ipam/model_ipv6networkcontainer_logic_filter_rules.go
@@ -0,0 +1,86 @@
+package ipam
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+
+	"github.com/Infoblox-CTO/infoblox-nios-go-client/ipam"
+
+	"github.com/Infoblox-CTO/infoblox-nios-terraform/internal/flex"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+)
+
+type Ipv6networkcontainerLogicFilterRulesModel struct {
+	Filter types.String `tfsdk:"filter"`
+	Type   types.String `tfsdk:"type"`
+}
+
+var Ipv6networkcontainerLogicFilterRulesAttrTypes = map[string]attr.Type{
+	"filter": types.StringType,
+	"type":   types.StringType,
+}
+
+var Ipv6networkcontainerLogicFilterRulesResourceSchemaAttributes = map[string]schema.Attribute{
+	"filter": schema.StringAttribute{
+		Optional:            true,
+		MarkdownDescription: "The filter name.",
+	},
+	"type": schema.StringAttribute{
+		Optional:            true,
+		MarkdownDescription: "The filter type. Valid values are: * MAC * NAC * Option",
+		Validators: []validator.String{
+			stringvalidator.OneOf("MAC", "NAC", "Option"),
+		},
+	},
+}
+
+func ExpandIpv6networkcontainerLogicFilterRules(ctx context.Context, o types.Object, diags *diag.Diagnostics) *ipam.Ipv6networkcontainerLogicFilterRules {
+	if o.IsNull() || o.IsUnknown() {
+		return nil
+	}
+	var m Ipv6networkcontainerLogicFilterRulesModel
+	diags.Append(o.As(ctx, &m, basetypes.ObjectAsOptions{})...)
+	if diags.HasError() {
+		return nil
+	}
+	return m.Expand(ctx, diags)
+}
+
+func (m *Ipv6networkcontainerLogicFilterRulesModel) Expand(ctx context.Context, diags *diag.Diagnostics) *ipam.Ipv6networkcontainerLogicFilterRules {
+	if m == nil {
+		return nil
+	}
+	to := &ipam.Ipv6networkcontainerLogicFilterRules{
+		Filter: flex.ExpandStringPointer(m.Filter),
+		Type:   flex.ExpandStringPointer(m.Type),
+	}
+	return to
+}
+
+func FlattenIpv6networkcontainerLogicFilterRules(ctx context.Context, from *ipam.Ipv6networkcontainerLogicFilterRules, diags *diag.Diagnostics) types.Object {
+	if from == nil {
+		return types.ObjectNull(Ipv6networkcontainerLogicFilterRulesAttrTypes)
+	}
+	m := Ipv6networkcontainerLogicFilterRulesModel{}
+	m.Flatten(ctx, from, diags)
+	t, d := types.ObjectValueFrom(ctx, Ipv6networkcontainerLogicFilterRulesAttrTypes, m)
+	diags.Append(d...)
+	return t
+}
+
+func (m *Ipv6networkcontainerLogicFilterRulesModel) Flatten(ctx context.Context, from *ipam.Ipv6networkcontainerLogicFilterRules, diags *diag.Diagnostics) {
+	if from == nil {
+		return
+	}
+	if m == nil {
+		*m = Ipv6networkcontainerLogicFilterRulesModel{}
+	}
+	m.Filter = flex.FlattenStringPointer(from.Filter)
+	m.Type = flex.FlattenStringPointer(from.Type)
+}
---
+++ internal/service/ipam/model_ipv6networkcontainersubscribesettings_mapped_ea_attributes.go
@@ -0,0 +1,81 @@
+package ipam
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+
+	"github.com/Infoblox-CTO/infoblox-nios-go-client/ipam"
+
+	"github.com/Infoblox-CTO/infoblox-nios-terraform/internal/flex"
+)
+
+type Ipv6networkcontainersubscribesettingsMappedEaAttributesModel struct {
+	Name     types.String `tfsdk:"name"`
+	MappedEa types.String `tfsdk:"mapped_ea"`
+}
+
+var Ipv6networkcontainersubscribesettingsMappedEaAttributesAttrTypes = map[string]attr.Type{
+	"name":      types.StringType,
+	"mapped_ea": types.StringType,
+}
+
+var Ipv6networkcontainersubscribesettingsMappedEaAttributesResourceSchemaAttributes = map[string]schema.Attribute{
+	"name": schema.StringAttribute{
+		Optional:            true,
+		MarkdownDescription: "The Cisco ISE attribute name that is enabled for publishsing from a Cisco ISE endpoint.",
+	},
+	"mapped_ea": schema.StringAttribute{
+		Optional:            true,
+		MarkdownDescription: "The name of the extensible attribute definition object the Cisco ISE attribute that is enabled for subscription is mapped on.",
+	},
+}
+
+func ExpandIpv6networkcontainersubscribesettingsMappedEaAttributes(ctx context.Context, o types.Object, diags *diag.Diagnostics) *ipam.Ipv6networkcontainersubscribesettingsMappedEaAttributes {
+	if o.IsNull() || o.IsUnknown() {
+		return nil
+	}
+	var m Ipv6networkcontainersubscribesettingsMappedEaAttributesModel
+	diags.Append(o.As(ctx, &m, basetypes.ObjectAsOptions{})...)
+	if diags.HasError() {
+		return nil
+	}
+	return m.Expand(ctx, diags)
+}
+
+func (m *Ipv6networkcontainersubscribesettingsMappedEaAttributesModel) Expand(ctx context.Context, diags *diag.Diagnostics) *ipam.Ipv6networkcontainersubscribesettingsMappedEaAttributes {
+	if m == nil {
+		return nil
+	}
+	to := &ipam.Ipv6networkcontainersubscribesettingsMappedEaAttributes{
+		Name:     flex.ExpandStringPointer(m.Name),
+		MappedEa: flex.ExpandStringPointer(m.MappedEa),
+	}
+	return to
+}
+
+func FlattenIpv6networkcontainersubscribesettingsMappedEaAttributes(ctx context.Context, from *ipam.Ipv6networkcontainersubscribesettingsMappedEaAttributes, diags *diag.Diagnostics) types.Object {
+	if from == nil {
+		return types.ObjectNull(Ipv6networkcontainersubscribesettingsMappedEaAttributesAttrTypes)
+	}
+	m := Ipv6networkcontainersubscribesettingsMappedEaAttributesModel{}
+	m.Flatten(ctx, from, diags)
+	t, d := types.ObjectValueFrom(ctx, Ipv6networkcontainersubscribesettingsMappedEaAttributesAttrTypes, m)
+	diags.Append(d...)
+	return t
+}
+
+func (m *Ipv6networkcontainersubscribesettingsMappedEaAttributesModel) Flatten(ctx context.Context, from *ipam.Ipv6networkcontainersubscribesettingsMappedEaAttributes, diags *diag.Diagnostics) {
+	if from == nil {
+		return
+	}
+	if m == nil {
+		*m = Ipv6networkcontainersubscribesettingsMappedEaAttributesModel{}
+	}
+	m.Name = flex.FlattenStringPointer(from.Name)
+	m.MappedEa = flex.FlattenStringPointer(from.MappedEa)
+}
