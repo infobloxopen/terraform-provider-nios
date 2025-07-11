4821217baa273658e5bdf2dfe2bcab9dfb9393fb
Jaykumar Chhatbar <45447272+JchhatbarInfoblox@users.noreply.github.com>
2025-07-11 18:33:01 +0530

    Update internal/service/ipam/networkcontainer_data_source.go
    Co-authored-by: Ujjwal Nasra <125353741+unasra@users.noreply.github.com>

--- internal/service/ipam/networkcontainer_data_source.go
+++ internal/service/ipam/networkcontainer_data_source.go
@@ -51,7 +51,7 @@ func (m *NetworkcontainerModelWithFilter) FlattenResults(ctx context.Context, fr
 
 func (d *NetworkcontainerDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		MarkdownDescription: "Manages a network container within Infoblox NIOS.",
+		MarkdownDescription: "Retrieves Information about existing Network Containers.",
 		Attributes: map[string]schema.Attribute{
 			"filters": schema.MapAttribute{
 				Description: "Filter are used to return a more specific list of results. Filters can be used to match resources by specific attributes, e.g. name. If you specify multiple filters, the results returned will have only resources that match all the specified filters.",
