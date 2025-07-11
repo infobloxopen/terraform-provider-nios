1778442f039a4507e9ef5bc1d364315a67cce0de
Jaykumar Chhatbar <45447272+JchhatbarInfoblox@users.noreply.github.com>
2025-07-11 18:40:19 +0530

    Update internal/service/ipam/networkcontainer_resource.go
    Co-authored-by: Vishruti Buddhadev <vbuddhadev@infoblox.com>

--- internal/service/ipam/networkcontainer_resource.go
+++ internal/service/ipam/networkcontainer_resource.go
@@ -37,7 +37,7 @@ func (r *NetworkcontainerResource) Metadata(ctx context.Context, req resource.Me
 
 func (r *NetworkcontainerResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		MarkdownDescription: "Manages a network container within Infoblox NIOS.",
+		MarkdownDescription: "Manages a Network Container",
 		Attributes:          NetworkcontainerResourceSchemaAttributes,
 	}
 }
