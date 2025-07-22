c68d7fcaaaf59c1112fc97d4b35506d4c53f2df4
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2025-07-22 18:04:05 +0530

    Generated Docs

--- examples/resources/nios_dns_zone_delegated/resource.tf
+++ examples/resources/nios_dns_zone_delegated/resource.tf
@@ -1,12 +1,12 @@
 // Create a DNS zone delegated with basic fields
 resource "nios_dns_zone_delegated" "zone_forward_basic_fields" {
-  fqdn              = "zone-delegated.example.com"
+  fqdn = "zone-delegated.example.com"
   delegate_to = [
-      {
-        name    = "ns1.example.com"
-        address = "1.1.1.1"
-      }
-    ]
+    {
+      name    = "ns1.example.com"
+      address = "1.1.1.1"
+    }
+  ]
 }
 
 // Create a DNS zone delegated with additional fields
@@ -20,14 +20,14 @@ resource "nios_dns_zone_delegated" "zone_forward_additional_fields" {
   ]
 
   // Additional fields
-  comment = "This is a delegated zone for example.com"
-  delegated_ttl    = 3600
+  comment           = "This is a delegated zone for example.com"
+  delegated_ttl     = 3600
   use_delegated_ttl = true
-  ms_ad_integrated = false
-  ms_ddns_mode = "ANY"
-  prefix = "example-prefix"
-  zone_format = "FORWARD"
-  view = "default"
+  ms_ad_integrated  = false
+  ms_ddns_mode      = "ANY"
+  prefix            = "example-prefix"
+  zone_format       = "FORWARD"
+  view              = "default"
 
   // Extensible Attributes
   extattrs = {
