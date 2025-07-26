2d9a54a32de18a641873c49e4b0a7b2a5d811208
apattar <apattar@infoblox.com>
2025-07-26 17:14:20 +0530

    NPA-436 fix

--- internal/service/dns/model_zone_forward.go
+++ internal/service/dns/model_zone_forward.go
@@ -2,13 +2,13 @@ package dns
 
 import (
 	"context"
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"regexp"
 
-	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/mapdefault"
@@ -143,6 +143,7 @@ var ZoneForwardResourceSchemaAttributes = map[string]schema.Attribute{
 			Attributes: ZoneForwardForwardToResourceSchemaAttributes,
 		},
 		Optional: true,
+		Computed: true,
 		Validators: []validator.List{
 			listvalidator.ConflictsWith(path.MatchRoot("external_ns_group")),
 		},
