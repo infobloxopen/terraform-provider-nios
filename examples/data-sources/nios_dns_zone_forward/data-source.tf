8f05daccbacbf35e56e5ca3b268def98e041918e
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2025-07-27 13:39:04 +0530

    Addressed Review Comments

--- examples/data-sources/nios_dns_zone_forward/data-source.tf
+++ examples/data-sources/nios_dns_zone_forward/data-source.tf
@@ -1,16 +1,16 @@
 // Retrieve a specific DNS zone forward by filters
-data "nios_dns_zone_forward" "get_zones_using_filters" {
+data "nios_dns_zone_forward" "get_zone_forward_using_filters" {
   filters = {
     fqdn = "zone-forward1.example.com"
   }
 }
 
 // Retrieve specific DNS zone forward using Extensible Attributes
-data "nios_dns_zone_forward" "get_zones_using_extensible_attributes" {
+data "nios_dns_zone_forward" "get_zone_forward_using_extensible_attributes" {
   extattrfilters = {
     Site = "location-1"
   }
 }
 
-// Retrieve all DNS zone forward zones
-data "nios_dns_zone_forward" "get_all_zones" {}
+// Retrieve all DNS zone forward
+data "nios_dns_zone_forward" "get_all_zone_forward" {}
