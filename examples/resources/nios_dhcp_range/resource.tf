0d81ead7ae1ac7ffefe5ee1470cc58a2d45ec80d
Chaithra007 <chaithra@infoblox.com>
2025-07-28 10:43:01 +0530

    addressing comment

--- examples/resources/nios_dhcp_range/resource.tf
+++ examples/resources/nios_dhcp_range/resource.tf
@@ -4,7 +4,7 @@ resource "nios_dhcp_range" "range_basic_fields" {
   end_addr   = "10.0.0.180"
 }
 
-//Create a Network Range with additional parameters 
+//Create a Network Range with additional fields 
 resource "nios_dhcp_range" "range_additional_fields" {
   start_addr        = "10.0.0.190"
   end_addr          = "10.0.0.200"
@@ -12,25 +12,6 @@ resource "nios_dhcp_range" "range_additional_fields" {
   network_view      = "default"
   name              = "range object"
   always_update_dns = true
-
-
-  // Email and notification settings
-  email_list     = ["admin@example.com", "network@example.com"]
-  use_email_list = true
-
-  // Water mark settings
-  high_water_mark       = 95
-  high_water_mark_reset = 85
-  low_water_mark        = 10
-  low_water_mark_reset  = 20
-
-  deny_all_clients = true
-  deny_bootp       = true
-  use_deny_bootp   = true
-
-  //DHCP Threshold
-  enable_dhcp_thresholds     = true
-  use_enable_dhcp_thresholds = true
 
   extattrs = {
     "Site" = "location-1"
@@ -41,38 +22,6 @@ resource "nios_dhcp_range" "range_additional_fields" {
     filter     = "finger_print_filter"
     permission = "Allow"
   }]
-  mac_filter_rules = [{
-    filter     = "mac_filter"
-    permission = "Deny"
-    }
-  ]
-  nac_filter_rules = [{
-    filter     = "nac_filter"
-    permission = "Deny"
-    }
-  ]
-  option_filter_rules = [
-    {
-      filter     = "option_filter"
-      permission = "Allow"
-    }
-  ]
-  relay_agent_filter_rules = [
-    {
-      filter     = "relay_agent_filter"
-      permission = "Allow"
-    }
-  ]
-  logic_filter_rules = [
-    {
-      filter = "mac_filter"
-      type   = "MAC"
-    }
-  ]
-  use_logic_filter_rules = true
-
-  ignore_dhcp_option_list_request     = true
-  use_ignore_dhcp_option_list_request = true
 
   nextserver     = "next_server.com"
   use_nextserver = true
@@ -84,5 +33,4 @@ resource "nios_dhcp_range" "range_additional_fields" {
   ignore_id     = "MACADDR"
   use_ignore_id = true
 
-  known_clients = "Allow"
 }
