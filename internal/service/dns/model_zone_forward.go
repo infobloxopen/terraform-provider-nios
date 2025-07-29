16144f5e5b946d5b39843466bc3fb961d163c6e2
apattar <apattar@infoblox.com>
2025-07-29 19:05:13 +0530

    NPA-645:adding custom validation for fqdn field

--- internal/service/dns/model_zone_forward.go
+++ internal/service/dns/model_zone_forward.go
@@ -18,6 +18,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 
 	"github.com/infobloxopen/infoblox-nios-go-client/dns"
+
 	"github.com/infobloxopen/terraform-provider-nios/internal/flex"
 	customvalidator "github.com/infobloxopen/terraform-provider-nios/internal/validator"
 )
@@ -166,8 +167,9 @@ var ZoneForwardResourceSchemaAttributes = map[string]schema.Attribute{
 		Required: true,
 		Validators: []validator.String{
 			customvalidator.IsValidFQDN(),
+			customvalidator.IsValidCidr(),
 		},
-		MarkdownDescription: "The name of this DNS zone. For a reverse zone, this is in \"address/cidr\" format. For other zones, this is in FQDN format. This value can be in unicode format. Note that for a reverse zone, the corresponding zone_format value should be set.",
+		MarkdownDescription: "The name of this DNS zone. For a reverse zone, this is in \"address/cidr\" format. For other zones, this is in FQDN format. This value can be in unicode format. Note that for a reverse zone, the corresponding zone_format value should be set. apra notation is not allowed for IP address.",
 	},
 	"locked": schema.BoolAttribute{
 		Optional:            true,
