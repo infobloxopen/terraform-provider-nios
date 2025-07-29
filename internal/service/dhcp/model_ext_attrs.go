fa16e49c4d8962086d6bb2fa9b9478e9ee01351d
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2025-07-29 15:52:23 +0530

    EA Changes for IPAM Objects

--- internal/service/dhcp/model_ext_attrs.go
+++ internal/service/dhcp/model_ext_attrs.go
@@ -1,128 +1,163 @@
-// Model functions for ExtAttrs
 package dhcp
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "strconv"
-    "strings"
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
 
-    "github.com/hashicorp/terraform-plugin-framework/attr"
-    "github.com/hashicorp/terraform-plugin-framework/diag"
-    "github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/go-uuid"
 
-    "github.com/infobloxopen/infoblox-nios-go-client/dhcp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/infobloxopen/infoblox-nios-go-client/dhcp"
+
+	"github.com/infobloxopen/terraform-provider-nios/internal/utils"
 )
 
-func ExpandExtAttr(ctx context.Context, extattrs types.Map, diags *diag.Diagnostics) *map[string]dhcp.ExtAttrs {
-    if extattrs.IsNull() || extattrs.IsUnknown() {
-        return nil
-    }
-    var extAttrsMap map[string]string
-    diags.Append(extattrs.ElementsAs(ctx, &extAttrsMap, false)...)
-    if diags.HasError() {
-        return nil
-    }
+const terraformInternalIDEA = "Terraform Internal ID"
 
-    result := make(map[string]dhcp.ExtAttrs)
+func ExpandExtAttrs(ctx context.Context, extattrs types.Map, diags *diag.Diagnostics) *map[string]dhcp.ExtAttrs {
+	if extattrs.IsNull() || extattrs.IsUnknown() {
+		return nil
+	}
+	var extAttrsMap map[string]string
+	diags.Append(extattrs.ElementsAs(ctx, &extAttrsMap, false)...)
+	if diags.HasError() {
+		return nil
+	}
 
-    for key, valStr := range extAttrsMap {
-        parsedValue := parseExtAttrValue(valStr)
-        result[key] = dhcp.ExtAttrs{Value: parsedValue}
-    }
-    return &result
+	result := make(map[string]dhcp.ExtAttrs)
+
+	for key, valStr := range extAttrsMap {
+		parsedValue := utils.ParseInterfaceValue(valStr)
+		result[key] = dhcp.ExtAttrs{Value: parsedValue}
+	}
+	return &result
 }
 
-func FlattenExtAttr(ctx context.Context, extattrs *map[string]dhcp.ExtAttrs, diags *diag.Diagnostics) types.Map {
-    result := make(map[string]attr.Value)
+func FlattenExtAttrs(ctx context.Context, planExtAttrs types.Map, extattrs *map[string]dhcp.ExtAttrs, diags *diag.Diagnostics) types.Map {
+	result := make(map[string]attr.Value)
+	planExtAttrsMap := planExtAttrs.Elements()
+	if extattrs == nil || len(*extattrs) == 0 {
+		return types.MapNull(types.StringType)
+	}
 
-    for key, extAttr := range *extattrs {
-        if extAttr.Value == nil {
-            continue
-        }
+	for key, extAttr := range *extattrs {
+		if extAttr.Value == nil {
+			continue
+		}
 
-        // Convert value to string based on its type
-        switch v := extAttr.Value.(type) {
-        case []interface{}:
-            // Convert list to JSON string
-            jsonBytes, err := json.Marshal(v)
-            if err != nil {
-                diags.AddError(
-                    "Error converting list to JSON",
-                    fmt.Sprintf("Could not convert list value for key %s: %s", key, err),
-                )
-                result[key] = types.StringValue(fmt.Sprintf("%v", v))
-            } else {
-                result[key] = types.StringValue(string(jsonBytes))
-            }
-        default:
-            // Convert primitive values to string
-            result[key] = types.StringValue(fmt.Sprintf("%v", v))
-        }
-    }
+		// Convert value to string based on its type
+		switch v := extAttr.Value.(type) {
+		case []interface{}:
+			// Convert list to JSON string
+			jsonBytes, err := json.Marshal(v)
+			if err != nil {
+				diags.AddError(
+					"Error converting list to JSON",
+					fmt.Sprintf("Could not convert list value for key %s: %s", key, err),
+				)
+				result[key] = types.StringValue(fmt.Sprintf("%v", v))
+			} else {
+				value := string(jsonBytes)
+				if _, ok := planExtAttrsMap[key]; ok {
+					if strings.Contains(planExtAttrsMap[key].String(), "'") {
+						value = strings.ReplaceAll(value, "\"", "'")
+					}
+				}
+				result[key] = types.StringValue(value)
+			}
+		default:
+			// Convert primitive values to string
+			result[key] = types.StringValue(fmt.Sprintf("%v", v))
+		}
+	}
 
-    mapVal, mapDiags := types.MapValue(types.StringType, result)
-    diags.Append(mapDiags...)
-    return mapVal
+	mapVal, mapDiags := types.MapValue(types.StringType, result)
+	diags.Append(mapDiags...)
+	return mapVal
 }
 
-func RemoveInheritedExtAttrs(ctx context.Context, planExtAttrs types.Map, respExtAttrs map[string]dhcp.ExtAttrs) (*map[string]dhcp.ExtAttrs, diag.Diagnostics) {
-    var diags diag.Diagnostics
-    newRespMap := make(map[string]dhcp.ExtAttrs, len(respExtAttrs))
+func RemoveInheritedExtAttrs(ctx context.Context, planExtAttrs types.Map, respExtAttrs map[string]dhcp.ExtAttrs) (*map[string]dhcp.ExtAttrs, types.Map, diag.Diagnostics) {
+	var diags diag.Diagnostics
+	extAttrsRespMap := make(map[string]dhcp.ExtAttrs, len(planExtAttrs.Elements()))
+	extAttrsAllRespMap := make(map[string]dhcp.ExtAttrs)
+	var extAttrAll types.Map
 
-    if planExtAttrs.IsNull() || planExtAttrs.IsUnknown() {
-        if v, ok := respExtAttrs["Terraform Internal ID"]; ok {
-            newRespMap["Terraform Internal ID"] = v
-        }
-        return &newRespMap, nil
-    }
+	if planExtAttrs.IsNull() || planExtAttrs.IsUnknown() {
+		extAttrAll = FlattenExtAttrs(ctx, planExtAttrs, &respExtAttrs, &diags)
+		return nil, extAttrAll, nil
+	}
 
-    planMap := *ExpandExtAttr(ctx, planExtAttrs, &diags)
-    if diags.HasError() {
-        return nil, diags
-    }
+	planMap := *ExpandExtAttrs(ctx, planExtAttrs, &diags)
+	if diags.HasError() {
+		return nil, extAttrAll, diags
+	}
 
-    for k, v := range respExtAttrs {
-        if k == "Terraform Internal ID" {
-            newRespMap[k] = v
-            continue
-        }
+	for k, v := range respExtAttrs {
+		if k == terraformInternalIDEA {
+			extAttrsAllRespMap[k] = v
+			continue
+		}
 
-        if respExtAttrs[k].AdditionalProperties["inheritance_source"] != nil {
-            if planVal, ok := planMap[k]; ok {
-                newRespMap[k] = planVal
-            }
-            continue
-        }
-        newRespMap[k] = respExtAttrs[k]
-    }
-    return &newRespMap, diags
+		// If the EA is inherited , if the state is override , add it to the ExtAttrs.
+		// If the EA is inherited and state is inherited , add it ExtAttrsAll
+		if respExtAttrs[k].AdditionalProperties["inheritance_source"] != nil {
+			if planVal, ok := planMap[k]; ok {
+				extAttrsRespMap[k] = planVal
+			} else {
+				extAttrsAllRespMap[k] = respExtAttrs[k]
+			}
+			continue
+		}
+		extAttrsRespMap[k] = v
+	}
+	extAttrAll = FlattenExtAttrs(ctx, planExtAttrs, &extAttrsAllRespMap, &diags)
+	return &extAttrsRespMap, extAttrAll, diags
 }
 
-func parseExtAttrValue(valStr string) interface{} {
-    // Check if the value appears to be a JSON array (enclosed in square brackets)
-    if strings.HasPrefix(valStr, "[") && strings.HasSuffix(valStr, "]") {
-        var listVal []interface{}
+func AddInheritedExtAttrs(ctx context.Context, planExtAttrs types.Map, stateExtAttrs types.Map) (types.Map, diag.Diagnostics) {
+	var diags diag.Diagnostics
+	stateExtAttrsMap := stateExtAttrs.Elements()
+	if len(stateExtAttrsMap) == 0 {
+		return planExtAttrs, diags
+	}
+	planExtAttrsMap := planExtAttrs.Elements()
 
-        // Parse as standard JSON with double quotes
-        err := json.Unmarshal([]byte(valStr), &listVal)
+	for k, v := range stateExtAttrsMap {
+		// if the key is not in planExtAttrsMap , we add it
+		if _, ok := planExtAttrsMap[k]; !ok {
+			planExtAttrsMap[k] = v
+		}
+	}
 
-        // If that fails and we have single quotes, replace them with double quotes
-        if err != nil && strings.Contains(valStr, "'") {
-            processedStr := strings.ReplaceAll(valStr, "'", "\"")
-            err = json.Unmarshal([]byte(processedStr), &listVal)
-        }
+	// Convert the updated map back to types.Map
+	newRespMap, diags := types.MapValue(types.StringType, planExtAttrsMap)
+	if diags.HasError() {
+		return planExtAttrs, diags
+	}
 
-        // If either parsing attempt succeeded, return the list value
-        if err == nil {
-            return listVal
-        }
-    }
+	return newRespMap, diags
+}
 
-    // Try to parse the value as an integer
-    if intVal, err := strconv.ParseInt(valStr, 10, 64); err == nil {
-        return intVal
-    }
-    return valStr
+func AddInternalIDToExtAttrs(ctx context.Context, extAttrs types.Map, diags diag.Diagnostics) (types.Map, diag.Diagnostics) {
+
+	internalId, err := uuid.GenerateUUID()
+	if err != nil {
+		diags.AddError("Error generating UUID", fmt.Sprintf("Unable to generate internal ID for Extensible Attributes: %s", err))
+		return extAttrs, diags
+	}
+
+	extAttrsMap := extAttrs.Elements()
+	extAttrsMap[terraformInternalIDEA] = types.StringValue(internalId)
+
+	extAttrs, diags = types.MapValue(types.StringType, extAttrsMap)
+	if diags.HasError() {
+		return extAttrs, diags
+	}
+
+	return extAttrs, nil
 }
