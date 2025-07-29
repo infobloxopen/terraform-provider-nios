c03fb5f432d3b7faffc11fb140e193e52b785851
Chaithra007 <chaithra@infoblox.com>
2025-07-29 16:13:36 +0530

    DHCP Options fix

--- internal/service/dhcp/model_rangetemplate_options.go
+++ internal/service/dhcp/model_rangetemplate_options.go
@@ -2,6 +2,7 @@ package dhcp
 
 import (
 	"context"
+
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
@@ -31,8 +32,7 @@ var RangetemplateOptionsAttrTypes = map[string]attr.Type{
 
 var RangetemplateOptionsResourceSchemaAttributes = map[string]schema.Attribute{
 	"name": schema.StringAttribute{
-		Optional:            true,
-		Computed:            true,
+		Required:            true,
 		MarkdownDescription: "Name of the DHCP option.",
 	},
 	"num": schema.Int64Attribute{
