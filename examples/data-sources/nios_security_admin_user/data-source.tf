caace17dd3171845c58135ebe4f1d657db0d213d
apattar <apattar@infoblox.com>
2025-08-11 20:36:25 +0530

    updating tests,docs and examples

---
+++ examples/data-sources/nios_security_admin_user/data-source.tf
@@ -0,0 +1,16 @@
+// Retrieve a specific Admin User View by filters
+data "nios_security_admin_user" "get_admin_user_using_filters" {
+  filters = {
+    name = "example_admin_user"
+  }
+}
+
+// Retrieve specific Admin Users using Extensible Attributes
+data "nios_security_admin_user" "get_admin_users_using_extensible_attributes" {
+  extattrfilters = {
+    Site = "location-1"
+  }
+}
+
+// Retrieve all Admin Users
+data "nios_security_admin_user" "get_all_admin_users" {}
