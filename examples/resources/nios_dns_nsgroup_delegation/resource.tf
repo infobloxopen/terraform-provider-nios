4772b46945151d8d73f7d80e78570673fb0b67c1
Chaithra007 <chaithra@infoblox.com>
2025-08-13 11:52:09 +0530

    added additional example

--- examples/resources/nios_dns_nsgroup_delegation/resource.tf
+++ examples/resources/nios_dns_nsgroup_delegation/resource.tf
@@ -1,12 +1,25 @@
+//Create an NS group delegation 
 resource "nios_dns_nsgroup_delegation" "create_nsgroup_delegate" {
-  name = "example_ns_group_delegation"
+  name = "example_ns_group_del"
   delegate_to = [
     {
       address = "2.3.3.4"
       name    = "delegate_to_ns_group"
-  }]
-  comment = "Example NS Group Delegation"
+    }
+  ]
+}
+
+//Create an NS group with additional attributes
+resource "nios_dns_nsgroup_delegation" "create_nsgroup_delegate_with_additional_params" {
+  name = "example_ns_group_delegation"
+  delegate_to = [
+    {
+      address = "2.3.3.5"
+      name    = "delegate_to_ns_group"
+    }
+  ]
+  comment = "Create NS Group Delegation"
   extattrs = {
-    Site = "location-1"
+    Site = "location-2"
   }
 }
