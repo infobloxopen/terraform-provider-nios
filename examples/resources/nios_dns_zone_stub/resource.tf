9b0aa95af58c53ff9139fdbe6bae9a92d01956ca
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2025-08-14 12:38:24 +0530

    Updated docs

--- examples/resources/nios_dns_zone_stub/resource.tf
+++ examples/resources/nios_dns_zone_stub/resource.tf
@@ -1,29 +1,29 @@
 // Create a DNS zone stub with basic fields
 resource "nios_dns_zone_stub" "zone_stub_basic_fields" {
-  fqdn              = "example_stub_zone.example.com"
+  fqdn = "example_stub_zone.example.com"
   stub_from = [
-      {
-        name    = "stub.example.com"
-        address = "1.1.1.1"
-      }
-    ]
+    {
+      name    = "stub.example.com"
+      address = "1.1.1.1"
+    }
+  ]
 }
 
 // Create a DNS zone stub with additional fields
 resource "nios_dns_zone_stub" "zone_stub_additional_fields" {
   fqdn = "example_stub_zone2.example.com"
   stub_from = [
-        {
-          name    = "stub.example.com"
-          address = "1.1.1.1"
-        }
-      ]
+    {
+      name    = "stub.example.com"
+      address = "1.1.1.1"
+    }
+  ]
 
   // Additional Fields
   ms_ddns_mode = "ANY"
-  prefix = "stub-prefix"
-  comment = "This is a stub zone with additional fields"
-  view = "default"
+  prefix       = "stub-prefix"
+  comment      = "This is a stub zone with additional fields"
+  view         = "default"
   extattrs = {
     Site = "location-1"
   }
