86a944c7aa2a167cc1d1d4814d04acaf23bb28b5
Chaithra007 <chaithra@infoblox.com>
2025-08-19 11:31:11 +0530

    added a validator

--- internal/service/dns/model_view.go
+++ internal/service/dns/model_view.go
@@ -624,6 +624,7 @@ var ViewResourceSchemaAttributes = map[string]schema.Attribute{
 	"nxdomain_redirect_addresses_v6": schema.ListAttribute{
 		ElementType: types.StringType,
 		Validators: []validator.List{
+			listvalidator.AlsoRequires(path.MatchRoot("use_nxdomain_redirect")),
 			listvalidator.SizeAtLeast(1),
 		},
 		Optional:            true,
