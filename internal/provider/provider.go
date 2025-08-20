39740cc72753708c6d6fdf92f2f6edb1fc63d8ec
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-08-20 15:18:49 +0530

    Ruleset Resource and Data Source implementation  (#98)
    * Initial commit for ruleset
    
    * wip
    
    * added missing descriptions
    
    * address review comments
    
    * resolve PR comments
    
    * resolve PR comments
    
    * wip
    
    * minor_changes
    
    * minor_change

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -17,6 +17,7 @@ import (
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/dns"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/dtc"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/ipam"
+	"github.com/infobloxopen/terraform-provider-nios/internal/service/misc"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/security"
 )
 
@@ -121,6 +122,8 @@ func (p *NIOSProvider) Resources(_ context.Context) []func() resource.Resource {
 		ipam.NewNetworkviewResource,
 
 		security.NewAdminroleResource,
+
+		misc.NewRulesetResource,
 	}
 }
 
@@ -157,6 +160,8 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 		ipam.NewNetworkviewDataSource,
 
 		security.NewAdminroleDataSource,
+
+		misc.NewRulesetDataSource,
 	}
 }
 
