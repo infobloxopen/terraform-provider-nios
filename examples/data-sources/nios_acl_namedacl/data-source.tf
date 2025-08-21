6377664afcfe1a1a295c11809a382efb9bcbd009
Raghav Sharma <rsharma6@ib-j91qjk64hx.inca.infoblox.com>
2025-08-21 19:40:35 +0530

    NPA-705: Added changes for namedacl resource and data source

---
+++ examples/data-sources/nios_acl_namedacl/data-source.tf
@@ -0,0 +1,16 @@
+// Retrieve a specific Named ACL by filters
+data "nios_acl_namedacl" "get_namedacl_using_filters" {
+  filters = {
+    name = "dev-network-acl"
+  }
+}
+
+// Retrieve specific Named ACL using Extensible Attributes
+data "nios_acl_namedacl" "get_namedacl_using_extensible_attributes" {
+  extattrfilters = {
+    Site = "location-1"
+  }
+}
+
+// Retrieve all Named ACLs
+data "nios_acl_namedacl" "get_all_namedacls" {}
---
+++ examples/resources/nios_acl_namedacl/resource.tf
@@ -0,0 +1,35 @@
+// Create Named Access Control Lists (ACLs) with basic fields
+resource "nios_acl_namedacl" "base_namedacl" {
+  name    = "base-acl-template"
+  comment = "Base ACL structure created for future assignment of access control entries"
+
+  // Extensible Attributes
+  extattrs = {
+    Site = "location-1"
+  }
+}
+
+// Create Named Access Control Lists (ACLs) with additional fields
+resource "nios_acl_namedacl" "namedacl_with_additional_config" {
+  name    = "dev-network-acl"
+  comment = "ACL to allow/deny access to specific dev network resources"
+
+  // ACL Entries
+  acl = [
+    {
+      struct     = "addressac"
+      address    = "10.0.0.1"
+      permission = "ALLOW"
+    },
+    {
+      struct     = "addressac"
+      address    = "10.0.0.2"
+      permission = "DENY"
+    }
+  ]
+
+  // Extensible Attributes
+  extattrs = {
+    Site = "location-2"
+  }
+}
