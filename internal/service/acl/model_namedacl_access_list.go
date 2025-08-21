88f0bd452cf6aa544ecab4e571f8b83a84bb9953
Raghav Sharma <rsharma6@ib-j91qjk64hx.inca.infoblox.com>
2025-08-21 19:40:35 +0530

    NPA-705: Added changes for namedacl resource and data source

---
+++ internal/service/acl/model_namedacl_access_list.go
@@ -0,0 +1,185 @@
+package acl
+
+import (
+	"context"
+	"regexp"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/boolvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+
+	"github.com/infobloxopen/infoblox-nios-go-client/acl"
+
+	"github.com/infobloxopen/terraform-provider-nios/internal/flex"
+)
+
+type NamedaclAccessListModel struct {
+	Struct         types.String `tfsdk:"struct"`
+	Address        types.String `tfsdk:"address"`
+	Permission     types.String `tfsdk:"permission"`
+	TsigKey        types.String `tfsdk:"tsig_key"`
+	TsigKeyAlg     types.String `tfsdk:"tsig_key_alg"`
+	TsigKeyName    types.String `tfsdk:"tsig_key_name"`
+	UseTsigKeyName types.Bool   `tfsdk:"use_tsig_key_name"`
+}
+
+var NamedaclAccessListAttrTypes = map[string]attr.Type{
+	"struct":            types.StringType,
+	"address":           types.StringType,
+	"permission":        types.StringType,
+	"tsig_key":          types.StringType,
+	"tsig_key_alg":      types.StringType,
+	"tsig_key_name":     types.StringType,
+	"use_tsig_key_name": types.BoolType,
+}
+
+var NamedaclAccessListResourceSchemaAttributes = map[string]schema.Attribute{
+	"struct": schema.StringAttribute{
+		Required: true,
+		Validators: []validator.String{
+			stringvalidator.OneOf("addressac", "tsigac"),
+		},
+		MarkdownDescription: "The struct type of the object. The value must be one of 'addressac' and 'tsigac'.",
+	},
+	"address": schema.StringAttribute{
+		Optional:            true,
+		Computed:            true,
+		MarkdownDescription: "The address this rule applies to or \"Any\".",
+		Validators: []validator.String{
+			stringvalidator.ConflictsWith(
+				path.MatchRelative().AtParent().AtName("tsig_key"),
+				path.MatchRelative().AtParent().AtName("tsig_key_alg"),
+				path.MatchRelative().AtParent().AtName("use_tsig_key_name"),
+			),
+			stringvalidator.RegexMatches(
+				regexp.MustCompile(`^[^\s].*[^\s]$`),
+				"Should not have leading or trailing whitespace",
+			),
+		},
+	},
+	"permission": schema.StringAttribute{
+		Optional:            true,
+		Computed:            true,
+		MarkdownDescription: "The permission to use for this address.",
+		Validators: []validator.String{
+			stringvalidator.ConflictsWith(
+				path.MatchRelative().AtParent().AtName("tsig_key"),
+				path.MatchRelative().AtParent().AtName("tsig_key_alg"),
+				path.MatchRelative().AtParent().AtName("use_tsig_key_name"),
+			),
+		},
+	},
+	"tsig_key": schema.StringAttribute{
+		Optional:            true,
+		Computed:            true,
+		MarkdownDescription: "A generated TSIG key. If the external primary server is a NIOS appliance running DNS One 2.x code, this can be set to :2xCOMPAT.",
+		Validators: []validator.String{
+			stringvalidator.ConflictsWith(
+				path.MatchRelative().AtParent().AtName("address"),
+				path.MatchRelative().AtParent().AtName("permission"),
+			),
+			stringvalidator.RegexMatches(
+				regexp.MustCompile(`^[^\s].*[^\s]$`),
+				"Should not have leading or trailing whitespace",
+			),
+		},
+	},
+	"tsig_key_alg": schema.StringAttribute{
+		Optional:            true,
+		Computed:            true,
+		MarkdownDescription: "The TSIG key algorithm.",
+		Validators: []validator.String{
+			stringvalidator.ConflictsWith(
+				path.MatchRelative().AtParent().AtName("address"),
+				path.MatchRelative().AtParent().AtName("permission"),
+			),
+		},
+	},
+	"tsig_key_name": schema.StringAttribute{
+		Optional:            true,
+		Computed:            true,
+		MarkdownDescription: "The name of the TSIG key. If 2.x TSIG compatibility is used, this is set to 'tsig_xfer' on retrieval, and ignored on insert or update.",
+		Validators: []validator.String{
+			stringvalidator.ConflictsWith(
+				path.MatchRelative().AtParent().AtName("address"),
+				path.MatchRelative().AtParent().AtName("permission"),
+			),
+			stringvalidator.RegexMatches(
+				regexp.MustCompile(`^[^\s].*[^\s]$`),
+				"Should not have leading or trailing whitespace",
+			),
+		},
+	},
+	"use_tsig_key_name": schema.BoolAttribute{
+		Optional:            true,
+		Computed:            true,
+		MarkdownDescription: "Use flag for: tsig_key_name",
+		Validators: []validator.Bool{
+			boolvalidator.ConflictsWith(
+				path.MatchRelative().AtParent().AtName("address"),
+				path.MatchRelative().AtParent().AtName("permission"),
+			),
+		},
+	},
+}
+
+func ExpandNamedaclAccessList(ctx context.Context, o types.Object, diags *diag.Diagnostics) *acl.NamedaclAccessList {
+	if o.IsNull() || o.IsUnknown() {
+		return nil
+	}
+	var m NamedaclAccessListModel
+	diags.Append(o.As(ctx, &m, basetypes.ObjectAsOptions{})...)
+	if diags.HasError() {
+		return nil
+	}
+	return m.Expand(ctx, diags)
+}
+
+func (m *NamedaclAccessListModel) Expand(ctx context.Context, diags *diag.Diagnostics) *acl.NamedaclAccessList {
+	if m == nil {
+		return nil
+	}
+	to := &acl.NamedaclAccessList{
+		Struct:         flex.ExpandStringPointer(m.Struct),
+		Address:        flex.ExpandStringPointer(m.Address),
+		Permission:     flex.ExpandStringPointer(m.Permission),
+		TsigKey:        flex.ExpandStringPointer(m.TsigKey),
+		TsigKeyAlg:     flex.ExpandStringPointer(m.TsigKeyAlg),
+		TsigKeyName:    flex.ExpandStringPointer(m.TsigKeyName),
+		UseTsigKeyName: flex.ExpandBoolPointer(m.UseTsigKeyName),
+	}
+	return to
+}
+
+func FlattenNamedaclAccessList(ctx context.Context, from *acl.NamedaclAccessList, diags *diag.Diagnostics) types.Object {
+	if from == nil {
+		return types.ObjectNull(NamedaclAccessListAttrTypes)
+	}
+	m := NamedaclAccessListModel{}
+	m.Flatten(ctx, from, diags)
+	t, d := types.ObjectValueFrom(ctx, NamedaclAccessListAttrTypes, m)
+	diags.Append(d...)
+	return t
+}
+
+func (m *NamedaclAccessListModel) Flatten(ctx context.Context, from *acl.NamedaclAccessList, diags *diag.Diagnostics) {
+	if from == nil {
+		return
+	}
+	if m == nil {
+		*m = NamedaclAccessListModel{}
+	}
+	m.Struct = flex.FlattenStringPointer(from.Struct)
+	m.Address = flex.FlattenStringPointer(from.Address)
+	m.Permission = flex.FlattenStringPointer(from.Permission)
+	m.TsigKey = flex.FlattenStringPointer(from.TsigKey)
+	m.TsigKeyAlg = flex.FlattenStringPointer(from.TsigKeyAlg)
+	m.TsigKeyName = flex.FlattenStringPointer(from.TsigKeyName)
+	m.UseTsigKeyName = types.BoolPointerValue(from.UseTsigKeyName)
+}
