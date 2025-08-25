e9d8932376fd0ca843c65ce40edab3309c6f0a62
Chaithra007 <chaithra@infoblox.com>
2025-08-25 22:37:04 +0530

    added validators for fields in ipam and dtc objects

--- internal/service/ipam/model_ipv6networkcontainerportcontrolblackoutsetting_blackout_schedule.go
+++ internal/service/ipam/model_ipv6networkcontainerportcontrolblackoutsetting_blackout_schedule.go
@@ -66,6 +66,7 @@ var Ipv6networkcontainerportcontrolblackoutsettingBlackoutScheduleResourceSchema
 					"SATURDAY",
 				),
 			),
+			listvalidator.SizeAtLeast(1),
 		},
 	},
 	"time_zone": schema.StringAttribute{
