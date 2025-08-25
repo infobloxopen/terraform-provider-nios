a07d77fadd600b72d73f6824ddd7f9dba9c81402
VishrutiBuddhadev <vbuddhadev@infoblox.com>
2025-08-25 10:50:31 +0530

    removed duplicate method

--- internal/service/dns/record_dname_resource_test.go
+++ internal/service/dns/record_dname_resource_test.go
@@ -685,14 +685,6 @@ resource "nios_dns_record_dname" "test_view" {
 	return strings.Join([]string{testAccBaseWithZone(zoneFqdn), config}, "")
 }
 
-func testAccBaseWithZone(zoneFqdn string) string {
-	return fmt.Sprintf(`
-resource "nios_dns_zone_auth" "test" {
-    fqdn = %q
-}
-`, zoneFqdn)
-}
-
 func testAccBaseWithTwoZones(zoneFqdn1, zoneFqdn2 string) string {
 	return fmt.Sprintf(`
 resource "nios_dns_zone_auth" "test" {
