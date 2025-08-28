d4f45dbbfafac57beb5cd3fd41ac504aae792cc2
VishrutiBuddhadev <vbuddhadev@infoblox.com>
2025-08-28 15:12:08 +0530

    addressed review comments

--- examples/resources/nios_dns_record_tlsa/resource.tf
+++ examples/resources/nios_dns_record_tlsa/resource.tf
@@ -8,8 +8,8 @@ resource "nios_dns_record_tlsa" "create_record_tlsa_with_basic_fields" {
   name              = "record-tlsa.${nios_dns_zone_auth.parent_zone.fqdn}"
   certificate_data  = "D2ABDE240D7CD3EE6B4B28C54DF034B97983A1D16E8A410E4561CB106618E971"
   certificate_usage = 2
-  matched_type      = 0
-  selector          = 0
+  matched_type      = 10
+  selector          = 20
 }
 
 // Create a TLSA record with Additional Fields
@@ -17,8 +17,8 @@ resource "nios_dns_record_tlsa" "create_record_tlsa_with_additional_fields" {
   name              = "record-tlsa-1.${nios_dns_zone_auth.parent_zone.fqdn}"
   certificate_data  = "D2ABDE240D7CD3EE6B4B28C54DF034B97983A1D16E8A410E4561CB106618E971"
   certificate_usage = 2
-  matched_type      = 0
-  selector          = 0
+  matched_type      = 10
+  selector          = 15
 
   // Additional Fields
   extattrs = {
