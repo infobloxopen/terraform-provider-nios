3bce9b4e3311fdf76cdcbf934323044d1fe40fb3
Chaithra007 <chaithra@infoblox.com>
2025-08-31 22:48:10 +0530

    addressing PR review comments

--- internal/service/dns/model_nsgroup_delegation.go
+++ internal/service/dns/model_nsgroup_delegation.go
@@ -2,10 +2,8 @@ package dns
 
 import (
 	"context"
-	"regexp"
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/mapvalidator"
-	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
@@ -16,6 +14,7 @@ import (
 
 	"github.com/infobloxopen/infoblox-nios-go-client/dns"
 	"github.com/infobloxopen/terraform-provider-nios/internal/flex"
+	customvalidator "github.com/infobloxopen/terraform-provider-nios/internal/validator"
 )
 
 type NsgroupDelegationModel struct {
@@ -45,10 +44,7 @@ var NsgroupDelegationResourceSchemaAttributes = map[string]schema.Attribute{
 		Optional: true,
 		Computed: true,
 		Validators: []validator.String{
-			stringvalidator.RegexMatches(
-				regexp.MustCompile(`^[^\s].*[^\s]$`),
-				"Should not have leading or trailing whitespace",
-			),
+			customvalidator.ValidateTrimmedString(),
 		},
 		Default : stringdefault.StaticString(""),
 		MarkdownDescription: "The comment for the delegated NS group.",
