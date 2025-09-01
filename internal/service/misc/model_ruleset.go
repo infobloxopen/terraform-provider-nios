9d834772b1ae754b6a13b23894ce2cde72941b4d
Chaithra007 <chaithra@infoblox.com>
2025-09-01 20:09:00 +0530

    fixed import issues

--- internal/service/misc/model_ruleset.go
+++ internal/service/misc/model_ruleset.go
@@ -11,11 +11,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 
 	"github.com/infobloxopen/infoblox-nios-go-client/misc"
 	"github.com/infobloxopen/terraform-provider-nios/internal/flex"
 	customvalidator "github.com/infobloxopen/terraform-provider-nios/internal/validator"
-	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 )
 
 type RulesetModel struct {
