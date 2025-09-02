965db2e42e806feac3f04fe82f9669046911b95a
Anil Gadiyar <agadiyarhj@infoblox.com>
2025-09-02 17:00:33 +0530

    fixed build issue

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -130,10 +130,9 @@ func (p *NIOSProvider) Resources(_ context.Context) []func() resource.Resource {
 
 		cloud.NewAwsuserResource,
 
-		misc.NewRulesetResource,
-
 		security.NewAdminroleResource,
-		security.NewAdminuserResource,
+
+		misc.NewRulesetResource,
 	}
 }
 
@@ -177,10 +176,9 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 
 		cloud.NewAwsuserDataSource,
 
-		misc.NewRulesetDataSource,
-
 		security.NewAdminroleDataSource,
-		security.NewAdminuserDataSource,
+
+		misc.NewRulesetDataSource,
 	}
 }
 
