de425ee40f3502d2e4edd37503b7217f20d91724
apattar <apattar@infoblox.com>
2025-09-03 13:16:21 +0530

    alias-record resource update

--- examples/resources/nios_dns_record_alias/resource.tf
+++ examples/resources/nios_dns_record_alias/resource.tf
@@ -22,3 +22,13 @@ resource "nios_dns_record_alias" "create_alias_record_with_additional_fields" {
   ttl     = 20
   use_ttl = true
 }
+
+import {
+  id = "record:alias/ZG5zLmFsaWFzX3JlY29yZCQuX2RlZmF1bHQuY29tLmluZm8uaW1wb3J0LWFsaWFzLk5BUFRS:import-alias.info.com/default"
+  to = nios_dns_record_alias.import4321
+}
+
+resource "nios_dns_record_alias" "import2" {
+
+
+}
