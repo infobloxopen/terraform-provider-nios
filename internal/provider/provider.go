a743c0d741c31c1571be33a2f3b5de7c096297dc
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-09-04 14:27:53 +0530

    [GRID] Upgradegroup Resource and Datasource Implementation (#123)
    * Initial commit for upgradegroup
    
    * wip
    
    * added test case for members
    
    * regenerated doc
    
    * addressed PR comments
    
    * address review comments
    
    * minor change
    
    * minor correction

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -11,12 +11,13 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	niosclient "github.com/infobloxopen/infoblox-nios-go-client/client"
-	"github.com/infobloxopen/infoblox-nios-go-client/grid"
+	gridclient "github.com/infobloxopen/infoblox-nios-go-client/grid"
 	"github.com/infobloxopen/infoblox-nios-go-client/option"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/acl"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/dhcp"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/dns"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/dtc"
+	"github.com/infobloxopen/terraform-provider-nios/internal/service/grid"
 	gridservice "github.com/infobloxopen/terraform-provider-nios/internal/service/grid"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/ipam"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/misc"
@@ -143,6 +144,7 @@ func (p *NIOSProvider) Resources(_ context.Context) []func() resource.Resource {
 		acl.NewNamedaclResource,
 
 		gridservice.NewNatgroupResource,
+		grid.NewUpgradegroupResource,
 	}
 }
 
@@ -197,6 +199,7 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 		acl.NewNamedaclDataSource,
 
 		gridservice.NewNatgroupDataSource,
+		grid.NewUpgradegroupDataSource,
 	}
 }
 
@@ -233,11 +236,11 @@ func checkAndCreatePreRequisites(ctx context.Context, client *niosclient.APIClie
 	}
 
 	// Create EA if it doesn't exist
-	data := grid.Extensibleattributedef{
-		Name:    grid.PtrString(terraformInternalIDEA),
-		Type:    grid.PtrString("STRING"),
-		Comment: grid.PtrString("Internal ID for Terraform Resource"),
-		Flags:   grid.PtrString("CR"),
+	data := gridclient.Extensibleattributedef{
+		Name:    gridclient.PtrString(terraformInternalIDEA),
+		Type:    gridclient.PtrString("STRING"),
+		Comment: gridclient.PtrString("Internal ID for Terraform Resource"),
+		Flags:   gridclient.PtrString("CR"),
 	}
 
 	_, _, err = client.GridAPI.ExtensibleattributedefAPI.
