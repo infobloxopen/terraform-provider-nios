912497983a927752eb46bcb1f54fbe7be27a9ea4
apattar <apattar@infoblox.com>
2025-09-10 18:32:02 +0530

    adding resource testcases1

--- examples/resources/nios_security_admin_group/resource.tf
+++ examples/resources/nios_security_admin_group/resource.tf
@@ -1,14 +1,20 @@
 // Create an Admin Group with Basic Fields
 resource "nios_security_admin_group" "admin_group_basic_fields" {
   name = "example_admin_group"
+  comment = "Example Admin Group with basic fields"
+  disable = true
+  extattrs = {
+    Site = "location-1"
+  }
+  admin_set_commands = {
+    set_collect_old_logs = true
+    set_core_files_quota = false
+  }
+  # access_method = ["GUI", "API"]
 }
 
 // Create an Admin Group with Additional Fields
 resource "nios_security_admin_group" "admin_group_additional_fields" {
   name    = "example_admin_group2"
-  comment = "Example Admin Group with additional fields"
-  disable = true
-  extattrs = {
-    Site = "location-1"
-  }
+  
 }
