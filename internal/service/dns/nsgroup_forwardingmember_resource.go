2458d9df6387ea01428dab6a2a4c40082608f8bc
Chaithra <chaithra@infoblox.com>
2025-09-12 13:20:32 +0530

    [DNS] Resource and Datasource Implementation for NS Group Forwarding Member  (#122)
    * model generation for nsgroup forwarding member
    
    * schema changes for ns group forwarding member
    
    * provider changes
    
    * Implementation for resource and datasource of NS group forwarding member
    
    * added datasource test cases
    
    * Merge branch 'main' into nsgroupForwardingMember
    
    * minor code changes
    
    * added TODO for test cases
    
    * removed unused function
    
    * new line at the end of resource.tf
    
    * added custom validator
    
    * formatted docs
    
    * addressing PR review comments
    
    * renaming nsgroup forwarding members
    
    * addressing PR comments
    
    * go generate
    
    * reverted the naming convention for ns group forwarding member

---
+++ internal/service/dns/nsgroup_forwardingmember_resource.go
@@ -0,0 +1,364 @@
+package dns
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+
+	niosclient "github.com/infobloxopen/infoblox-nios-go-client/client"
+
+	"github.com/infobloxopen/terraform-provider-nios/internal/utils"
+)
+
+var readableAttributesForNsgroupForwardingmember = "comment,extattrs,forwarding_servers,name"
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &NsgroupForwardingmemberResource{}
+var _ resource.ResourceWithImportState = &NsgroupForwardingmemberResource{}
+
+func NewNsgroupForwardingmemberResource() resource.Resource {
+	return &NsgroupForwardingmemberResource{}
+}
+
+// NsgroupForwardingmemberResource defines the resource implementation.
+type NsgroupForwardingmemberResource struct {
+	client *niosclient.APIClient
+}
+
+func (r *NsgroupForwardingmemberResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + "dns_nsgroup_forwardingmember"
+}
+
+func (r *NsgroupForwardingmemberResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Manages an NS Group Forwarding Member.",
+		Attributes:          NsgroupForwardingmemberResourceSchemaAttributes,
+	}
+}
+
+func (r *NsgroupForwardingmemberResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*niosclient.APIClient)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *niosclient.APIClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *NsgroupForwardingmemberResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var diags diag.Diagnostics
+	var data NsgroupForwardingmemberModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Add internal ID exists in the Extensible Attributes if not already present
+	data.ExtAttrs, diags = AddInternalIDToExtAttrs(ctx, data.ExtAttrs, diags)
+	if diags.HasError() {
+		return
+	}
+
+	apiRes, _, err := r.client.DNSAPI.
+		NsgroupForwardingmemberAPI.
+		Create(ctx).
+		NsgroupForwardingmember(*data.Expand(ctx, &resp.Diagnostics)).
+		ReturnFieldsPlus(readableAttributesForNsgroupForwardingmember).
+		ReturnAsObject(1).
+		Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create NsgroupForwardingmember, got error: %s", err))
+		return
+	}
+
+	res := apiRes.CreateNsgroupForwardingmemberResponseAsObject.GetResult()
+	res.ExtAttrs, data.ExtAttrsAll, diags = RemoveInheritedExtAttrs(ctx, data.ExtAttrs, *res.ExtAttrs)
+	if diags.HasError() {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Error while create NsgroupForwardingmember due inherited Extensible attributes, got error: %s", err))
+		return
+	}
+
+	data.Flatten(ctx, &res, &resp.Diagnostics)
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *NsgroupForwardingmemberResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var diags diag.Diagnostics
+	var data NsgroupForwardingmemberModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	apiRes, httpRes, err := r.client.DNSAPI.
+		NsgroupForwardingmemberAPI.
+		Read(ctx, utils.ExtractResourceRef(data.Ref.ValueString())).
+		ReturnFieldsPlus(readableAttributesForNsgroupForwardingmember).
+		ReturnAsObject(1).
+		Execute()
+
+	// If the resource is not found, try searching using Extensible Attributes
+	if err != nil {
+		if httpRes != nil && httpRes.StatusCode == http.StatusNotFound && r.ReadByExtAttrs(ctx, &data, resp) {
+			return
+		}
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read NsgroupForwardingmember, got error: %s", err))
+		return
+	}
+
+	res := apiRes.GetNsgroupForwardingmemberResponseObjectAsResult.GetResult()
+
+	apiTerraformId, ok := (*res.ExtAttrs)[terraformInternalIDEA]
+	if !ok {
+		apiTerraformId.Value = ""
+	}
+
+	stateExtAttrs := ExpandExtAttrs(ctx, data.ExtAttrsAll, &diags)
+	if stateExtAttrs == nil {
+		resp.Diagnostics.AddError(
+			"Missing Internal ID",
+			"Unable to read NsgroupForwardingmember because the internal ID (from extattrs_all) is missing or invalid.",
+		)
+		return
+	}
+
+	stateTerraformId := (*stateExtAttrs)[terraformInternalIDEA]
+	if apiTerraformId.Value != stateTerraformId.Value {
+		if r.ReadByExtAttrs(ctx, &data, resp) {
+			return
+		}
+	}
+
+	res.ExtAttrs, data.ExtAttrsAll, diags = RemoveInheritedExtAttrs(ctx, data.ExtAttrs, *res.ExtAttrs)
+	if diags.HasError() {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Error while reading NsgroupForwardingmember due inherited Extensible attributes, got error: %s", diags))
+		return
+	}
+
+	data.Flatten(ctx, &res, &resp.Diagnostics)
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *NsgroupForwardingmemberResource) ReadByExtAttrs(ctx context.Context, data *NsgroupForwardingmemberModel, resp *resource.ReadResponse) bool {
+	var diags diag.Diagnostics
+
+	if data.ExtAttrsAll.IsNull() {
+		return false
+	}
+
+	internalIdExtAttr := *ExpandExtAttrs(ctx, data.ExtAttrsAll, &diags)
+	if diags.HasError() {
+		return false
+	}
+
+	internalId := internalIdExtAttr[terraformInternalIDEA].Value
+	if internalId == "" {
+		return false
+	}
+
+	idMap := map[string]interface{}{
+		terraformInternalIDEA: internalId,
+	}
+
+	apiRes, _, err := r.client.DNSAPI.
+		NsgroupForwardingmemberAPI.
+		List(ctx).
+		Extattrfilter(idMap).
+		ReturnAsObject(1).
+		ReturnFieldsPlus(readableAttributesForNsgroupForwardingmember).
+		Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read NsgroupForwardingmember by extattrs, got error: %s", err))
+		return true
+	}
+
+	results := apiRes.ListNsgroupForwardingmemberResponseObject.GetResult()
+
+	// If the list is empty, the resource no longer exists so remove it from state
+	if len(results) == 0 {
+		resp.State.RemoveResource(ctx)
+		return true
+	}
+
+	res := results[0]
+
+	// Remove inherited external attributes from extattrs
+	res.ExtAttrs, data.ExtAttrsAll, diags = RemoveInheritedExtAttrs(ctx, data.ExtAttrs, *res.ExtAttrs)
+	if diags.HasError() {
+		return true
+	}
+
+	data.Flatten(ctx, &res, &resp.Diagnostics)
+	resp.Diagnostics.Append(resp.State.Set(ctx, data)...)
+
+	return true
+}
+
+func (r *NsgroupForwardingmemberResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var diags diag.Diagnostics
+	var data NsgroupForwardingmemberModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	planExtAttrs := data.ExtAttrs
+	diags = req.State.GetAttribute(ctx, path.Root("ref"), &data.Ref)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+
+	diags = req.State.GetAttribute(ctx, path.Root("extattrs_all"), &data.ExtAttrsAll)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+
+	// Add Inherited Extensible Attributes
+	data.ExtAttrs, diags = AddInheritedExtAttrs(ctx, data.ExtAttrs, data.ExtAttrsAll)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+
+	apiRes, _, err := r.client.DNSAPI.
+		NsgroupForwardingmemberAPI.
+		Update(ctx, utils.ExtractResourceRef(data.Ref.ValueString())).
+		NsgroupForwardingmember(*data.Expand(ctx, &resp.Diagnostics)).
+		ReturnFieldsPlus(readableAttributesForNsgroupForwardingmember).
+		ReturnAsObject(1).
+		Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update NsgroupForwardingmember, got error: %s", err))
+		return
+	}
+
+	res := apiRes.UpdateNsgroupForwardingmemberResponseAsObject.GetResult()
+
+	res.ExtAttrs, data.ExtAttrsAll, diags = RemoveInheritedExtAttrs(ctx, planExtAttrs, *res.ExtAttrs)
+	if diags.HasError() {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Error while update NsgroupForwardingmember due inherited Extensible attributes, got error: %s", diags))
+		return
+	}
+
+	data.Flatten(ctx, &res, &resp.Diagnostics)
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *NsgroupForwardingmemberResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data NsgroupForwardingmemberModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	httpRes, err := r.client.DNSAPI.
+		NsgroupForwardingmemberAPI.
+		Delete(ctx, utils.ExtractResourceRef(data.Ref.ValueString())).
+		Execute()
+	if err != nil {
+		if httpRes != nil && httpRes.StatusCode == http.StatusNotFound {
+			return
+		}
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete NsgroupForwardingmember, got error: %s", err))
+		return
+	}
+}
+
+func (r *NsgroupForwardingmemberResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	var diags diag.Diagnostics
+	var data NsgroupForwardingmemberModel
+
+	resourceRef := utils.ExtractResourceRef(req.ID)
+
+	apiRes, _, err := r.client.DNSAPI.
+		NsgroupForwardingmemberAPI.
+		Read(ctx, resourceRef).
+		ReturnFieldsPlus(readableAttributesForNsgroupForwardingmember).
+		ReturnAsObject(1).
+		Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Import Failed", fmt.Sprintf("Cannot read NsgroupForwardingmember for import, got error: %s", err))
+		return
+	}
+
+	res := apiRes.GetNsgroupForwardingmemberResponseObjectAsResult.GetResult()
+
+	res.ExtAttrs, data.ExtAttrsAll, diags = RemoveInheritedExtAttrs(ctx, data.ExtAttrs, *res.ExtAttrs)
+	if diags.HasError() {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Error while reading NsgroupForwardingmember for import due inherited Extensible attributes, got error: %s", diags))
+		return
+	}
+
+	data.Flatten(ctx, &res, &resp.Diagnostics)
+
+	planExtAttrs := data.ExtAttrs
+	data.ExtAttrs, diags = AddInheritedExtAttrs(ctx, data.ExtAttrs, data.ExtAttrsAll)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+
+	data.ExtAttrs, diags = AddInternalIDToExtAttrs(ctx, data.ExtAttrs, diags)
+	if diags.HasError() {
+		return
+	}
+
+	updateRes, _, err := r.client.DNSAPI.
+		NsgroupForwardingmemberAPI.
+		Update(ctx, resourceRef).
+		NsgroupForwardingmember(*data.Expand(ctx, &resp.Diagnostics)).
+		ReturnFieldsPlus(readableAttributesForNsgroupForwardingmember).
+		ReturnAsObject(1).
+		Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("Import Failed", fmt.Sprintf("Unable to update NsgroupForwardingmember for import, got error: %s", err))
+		return
+	}
+
+	res = updateRes.UpdateNsgroupForwardingmemberResponseAsObject.GetResult()
+
+	res.ExtAttrs, data.ExtAttrsAll, diags = RemoveInheritedExtAttrs(ctx, planExtAttrs, *res.ExtAttrs)
+	if diags.HasError() {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Error while update NsgroupForwardingmember due inherited Extensible attributes for import, got error: %s", diags))
+		return
+	}
+	data.Flatten(ctx, &res, &resp.Diagnostics)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
---
+++ internal/service/dns/nsgroup_forwardingmember_resource_test.go
@@ -0,0 +1,352 @@
+package dns_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
+	"github.com/infobloxopen/infoblox-nios-go-client/dns"
+	"github.com/infobloxopen/terraform-provider-nios/internal/acctest"
+	"github.com/infobloxopen/terraform-provider-nios/internal/utils"
+)
+
+// TODO: OBJECTS TO BE PRESENT IN GRID FOR TESTS
+// A NIOS grid with 1 members - member.com
+var readableAttributesForNsgroupForwardingmember = "comment,extattrs,forwarding_servers,name"
+
+func TestAccNsgroupForwardingmemberResource_basic(t *testing.T) {
+	var resourceName = "nios_dns_nsgroup_forwardingmember.test"
+	var v dns.NsgroupForwardingmember
+	name := acctest.RandomNameWithPrefix("ns-group-forwardingMember")
+	forwardingServers := []map[string]any{
+		{
+			"name": "infoblox.localdomain",
+		},
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read
+			{
+				Config: testAccNsgroupForwardingmemberBasicConfig(name, forwardingServers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "forwarding_servers.0.name", "infoblox.localdomain"),
+					// Test fields with default value
+					resource.TestCheckResourceAttr(resourceName, "comment", ""),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func TestAccNsgroupForwardingmemberResource_disappears(t *testing.T) {
+	resourceName := "nios_dns_nsgroup_forwardingmember.test"
+	var v dns.NsgroupForwardingmember
+	name := acctest.RandomNameWithPrefix("ns-group-forwardingMember")
+	forwardingServers := []map[string]any{
+		{
+			"name": "infoblox.localdomain",
+		},
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckNsgroupForwardingmemberDestroy(context.Background(), &v),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNsgroupForwardingmemberBasicConfig(name, forwardingServers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					testAccCheckNsgroupForwardingmemberDisappears(context.Background(), &v),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func TestAccNsgroupForwardingmemberResource_Comment(t *testing.T) {
+	var resourceName = "nios_dns_nsgroup_forwardingmember.test_comment"
+	var v dns.NsgroupForwardingmember
+	name := acctest.RandomNameWithPrefix("ns-group-forwardingMember")
+	forwardingServers := []map[string]any{
+		{
+			"name": "infoblox.localdomain",
+		},
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read
+			{
+				Config: testAccNsgroupForwardingmemberComment(name, forwardingServers, "this is an comment"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "comment", "this is an comment"),
+				),
+			},
+			// Update and Read
+			{
+				Config: testAccNsgroupForwardingmemberComment(name, forwardingServers, "this is an updated comment"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "comment", "this is an updated comment"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func TestAccNsgroupForwardingmemberResource_ExtAttrs(t *testing.T) {
+	var resourceName = "nios_dns_nsgroup_forwardingmember.test_extattrs"
+	var v dns.NsgroupForwardingmember
+	name := acctest.RandomNameWithPrefix("ns-group-forwardingMember")
+	forwardingServers := []map[string]any{
+		{
+			"name": "infoblox.localdomain",
+		},
+	}
+	extAttrValue1 := acctest.RandomName()
+	extAttrValue2 := acctest.RandomName()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read
+			{
+				Config: testAccNsgroupForwardingmemberExtAttrs(name, forwardingServers, map[string]string{
+					"Site": extAttrValue1,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "extattrs.Site", extAttrValue1),
+				),
+			},
+			// Update and Read
+			{
+				Config: testAccNsgroupForwardingmemberExtAttrs(name, forwardingServers, map[string]string{
+					"Site": extAttrValue2,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "extattrs.Site", extAttrValue2),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func TestAccNsgroupForwardingmemberResource_ForwardingServers(t *testing.T) {
+	var resourceName = "nios_dns_nsgroup_forwardingmember.test_forwarding_servers"
+	var v dns.NsgroupForwardingmember
+	name := acctest.RandomNameWithPrefix("ns-group-forwardingMember")
+	forwardingServers := []map[string]any{
+		{
+			"name": "infoblox.localdomain",
+		},
+	}
+	forwardingServersUpdate := []map[string]any{
+		{
+			"name":                    "member.com",
+			"use_override_forwarders": true,
+			"forward_to": []map[string]any{
+				{
+					"name":    "forwarder.com",
+					"address": "2.3.4.5",
+				},
+			},
+		},
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read
+			{
+				Config: testAccNsgroupForwardingmemberForwardingServers(name, forwardingServers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "forwarding_servers.0.name", "infoblox.localdomain"),
+				),
+			},
+			// Update and Read
+			{
+				Config: testAccNsgroupForwardingmemberForwardingServers(name, forwardingServersUpdate),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "forwarding_servers.0.name", "member.com"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func TestAccNsgroupForwardingmemberResource_Name(t *testing.T) {
+	var resourceName = "nios_dns_nsgroup_forwardingmember.test_name"
+	var v dns.NsgroupForwardingmember
+	name := acctest.RandomNameWithPrefix("ns-group-forwardingMember")
+	nameUpdate := acctest.RandomNameWithPrefix("ns-group-forwardingMember")
+	forwardingServers := []map[string]any{
+		{
+			"name": "infoblox.localdomain",
+		},
+	}
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read
+			{
+				Config: testAccNsgroupForwardingmemberName(name, forwardingServers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+				),
+			},
+			// Update and Read
+			{
+				Config: testAccNsgroupForwardingmemberName(nameUpdate, forwardingServers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsgroupForwardingmemberExists(context.Background(), resourceName, &v),
+					resource.TestCheckResourceAttr(resourceName, "name", nameUpdate),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccCheckNsgroupForwardingmemberExists(ctx context.Context, resourceName string, v *dns.NsgroupForwardingmember) resource.TestCheckFunc {
+	// Verify the resource exists in the cloud
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("not found: %s", resourceName)
+		}
+		apiRes, _, err := acctest.NIOSClient.DNSAPI.
+			NsgroupForwardingmemberAPI.
+			Read(ctx, utils.ExtractResourceRef(rs.Primary.Attributes["ref"])).
+			ReturnFieldsPlus(readableAttributesForNsgroupForwardingmember).
+			ReturnAsObject(1).
+			Execute()
+		if err != nil {
+			return err
+		}
+		if !apiRes.GetNsgroupForwardingmemberResponseObjectAsResult.HasResult() {
+			return fmt.Errorf("expected result to be returned: %s", resourceName)
+		}
+		*v = apiRes.GetNsgroupForwardingmemberResponseObjectAsResult.GetResult()
+		return nil
+	}
+}
+
+func testAccCheckNsgroupForwardingmemberDestroy(ctx context.Context, v *dns.NsgroupForwardingmember) resource.TestCheckFunc {
+	// Verify the resource was destroyed
+	return func(state *terraform.State) error {
+		_, httpRes, err := acctest.NIOSClient.DNSAPI.
+			NsgroupForwardingmemberAPI.
+			Read(ctx, utils.ExtractResourceRef(*v.Ref)).
+			ReturnAsObject(1).
+			ReturnFieldsPlus(readableAttributesForNsgroupForwardingmember).
+			Execute()
+		if err != nil {
+			if httpRes != nil && httpRes.StatusCode == http.StatusNotFound {
+				// resource was deleted
+				return nil
+			}
+			return err
+		}
+		return errors.New("expected to be deleted")
+	}
+}
+
+func testAccCheckNsgroupForwardingmemberDisappears(ctx context.Context, v *dns.NsgroupForwardingmember) resource.TestCheckFunc {
+	// Delete the resource externally to verify disappears test
+	return func(state *terraform.State) error {
+		_, err := acctest.NIOSClient.DNSAPI.
+			NsgroupForwardingmemberAPI.
+			Delete(ctx, utils.ExtractResourceRef(*v.Ref)).
+			Execute()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func testAccNsgroupForwardingmemberBasicConfig(name string, forwardingServer []map[string]any) string {
+	forwardingServerStr := utils.ConvertSliceOfMapsToHCL(forwardingServer)
+	return fmt.Sprintf(`
+resource "nios_dns_nsgroup_forwardingmember" "test" {
+	name = %q
+	forwarding_servers = %s
+}
+`, name, forwardingServerStr)
+}
+
+func testAccNsgroupForwardingmemberComment(name string, forwardingServer []map[string]any, comment string) string {
+	forwardingServerStr := utils.ConvertSliceOfMapsToHCL(forwardingServer)
+	return fmt.Sprintf(`
+resource "nios_dns_nsgroup_forwardingmember" "test_comment" {
+    name = %q
+    forwarding_servers = %s
+    comment = %q
+}
+`, name, forwardingServerStr, comment)
+}
+
+func testAccNsgroupForwardingmemberExtAttrs(name string, forwardingServer []map[string]any, extAttrs map[string]string) string {
+	extattrsStr := "{\n"
+	for k, v := range extAttrs {
+		extattrsStr += fmt.Sprintf(`
+  %s = %q
+`, k, v)
+	}
+	extattrsStr += "\t}"
+	forwardingServerStr := utils.ConvertSliceOfMapsToHCL(forwardingServer)
+	return fmt.Sprintf(`
+resource "nios_dns_nsgroup_forwardingmember" "test_extattrs" {
+	name = %q
+	forwarding_servers = %s
+    extattrs = %s
+}
+`, name, forwardingServerStr, extattrsStr)
+}
+
+func testAccNsgroupForwardingmemberForwardingServers(name string, forwardingServers []map[string]any) string {
+	forwardingServersStr := utils.ConvertSliceOfMapsToHCL(forwardingServers)
+	return fmt.Sprintf(`
+resource "nios_dns_nsgroup_forwardingmember" "test_forwarding_servers" {
+    name = %q
+    forwarding_servers = %s
+}
+`, name, forwardingServersStr)
+}
+
+func testAccNsgroupForwardingmemberName(name string, forwardingServers []map[string]any) string {
+	forwardingServersStr := utils.ConvertSliceOfMapsToHCL(forwardingServers)
+	return fmt.Sprintf(`
+resource "nios_dns_nsgroup_forwardingmember" "test_name" {
+    name = %q
+    forwarding_servers = %s
+}
+`, name, forwardingServersStr)
+}
