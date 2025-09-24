28a0ef6621eb2de8bb155634824a4645454d7fc6
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-09-24 18:42:46 +0530

    removed unused import

--- internal/service/grid/upgradegroup_resource.go
+++ internal/service/grid/upgradegroup_resource.go
@@ -12,7 +12,6 @@ import (
 
 	niosclient "github.com/infobloxopen/infoblox-nios-go-client/client"
 
-	//"github.com/infobloxopen/terraform-provider-nios/internal/flex"
 	"github.com/infobloxopen/terraform-provider-nios/internal/utils"
 )
 
