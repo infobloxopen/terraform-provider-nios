76a8108dcaf056a3e2a4627808e8cde43a0bb775
Anil Gadiyar <agadiyarhj@infoblox.com>
2025-09-25 16:31:41 +0530

    wip

--- examples/data-sources/nios_security_ftpuser/data-source.tf
+++ examples/data-sources/nios_security_ftpuser/data-source.tf
@@ -1,14 +1,14 @@
-// Retrieve a specific ftp user by filters
+// Retrieve a specific FTP user by filters
 data "nios_security_ftpuser" "get_ftpuser_using_filters" {
   filters = {
     username = "example_ftpuser"
   }
 }
-// Retrieve specific ftp users using Extensible Attributes
+// Retrieve specific FTP users using Extensible Attributes
 data "nios_security_ftpuser" "get_ftpusers_using_extensible_attributes" {
   extattrfilters = {
     Site = "location-1"
   }
 }
-// Retrieve all ftp users
+// Retrieve all FTP users
 data "nios_security_ftpuser" "get_all_ftpusers" {}
