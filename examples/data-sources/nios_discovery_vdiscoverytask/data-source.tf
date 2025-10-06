1341ac8233da0a2e153646d10de276dea336a31e
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-10-06 11:34:57 +0530

    removed older docs and examples

--- examples/data-sources/nios_discovery_vdiscoverytask/data-source.tf
+++
@@ -1,9 +0,0 @@
-// Retrieve a specifi Vdiscoverytask by filters
-data "nios_discovery_vdiscoverytask" "get_vdiscoverytask_using_filters" {
-  filters = {
-    name = "AWS-Vdiscoverytask"
-  }
-}
-
-// Retrieve all Vdiscoverytasks
-data "nios_discovery_vdiscoverytask" "get_all_vdiscoverytasks" {}
