83f11acd2b12357aafee8fc15eae351a5ae62329
Anil Gadiyar <agadiyarhj@infoblox.com>
2025-10-21 16:58:18 +0530

    addressed pr comment

--- examples/resources/nios_dns_record_txt/resource.tf
+++ examples/resources/nios_dns_record_txt/resource.tf
@@ -6,43 +6,6 @@ resource "nios_dns_zone_auth" "parent_auth_zone" {
   comment     = "Parent zone for SRV and TXT records"
 }
 
-// Create Record SRV with Basic Fields
-resource "nios_dns_record_srv" "create_record" {
-  name     = "example-srv-record.${nios_dns_zone_auth.parent_auth_zone.fqdn}"
-  target   = "example.target.${nios_dns_zone_auth.parent_auth_zone.fqdn}"
-  port     = 80
-  priority = 4
-  weight   = 50
-
-  // Extensible Attributes
-  extattrs = {
-    Site = "location-1"
-  }
-  depends_on = [nios_dns_zone_auth.parent_auth_zone]
-}
-
-// Create Record SRV with additional fields
-resource "nios_dns_record_srv" "create_with_additional_config" {
-  name     = "example-srv-record-with-config.${nios_dns_zone_auth.parent_auth_zone.fqdn}"
-  target   = "example_updated.target.${nios_dns_zone_auth.parent_auth_zone.fqdn}"
-  port     = 8080
-  priority = 2
-  weight   = 100
-
-  // Additional Fields
-  view    = "default"
-  use_ttl = true
-  ttl     = 10
-  creator = "DYNAMIC"
-  comment = "Example SRV record"
-
-  // Extensible Attributes
-  extattrs = {
-    Site = "location-2"
-  }
-  depends_on = [nios_dns_zone_auth.parent_auth_zone]
-}
-
 // Create Record TXT with Basic Fields
 resource "nios_dns_record_txt" "create_record" {
   name = "example-txt-record.${nios_dns_zone_auth.parent_auth_zone.fqdn}"
@@ -52,7 +15,6 @@ resource "nios_dns_record_txt" "create_record" {
   extattrs = {
     Site = "location-1"
   }
-  depends_on = [nios_dns_zone_auth.parent_auth_zone]
 }
 
 // Create Record TXT with additional fields
@@ -71,5 +33,4 @@ resource "nios_dns_record_txt" "create_with_additional_config" {
   extattrs = {
     Site = "location-2"
   }
-  depends_on = [nios_dns_zone_auth.parent_auth_zone]
 }
