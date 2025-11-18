d1ee4031c697287b639153bb7c35fb31351ba67a
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-11-18 11:47:33 +0530

    added parent for the shared record srv

--- examples/resources/nios_dns_sharedrecord_srv/resource.tf
+++ examples/resources/nios_dns_sharedrecord_srv/resource.tf
@@ -1,9 +1,14 @@
+// Create Parent Shared Record Group
+resource "nios_dns_sharedrecordgroup" "parent_sharedrecordgroup" {
+  name = "example-sharedrecordgroup"
+}
+
 // Create a Shared SRV Record with Basic Fields
 resource "nios_dns_sharedrecord_srv" "sharedrecord_srv_basic_fields" {
   name                = "sharedrecord_srv.example.com"
   port                = 443
   priority            = 10
-  shared_record_group = "example-sharedrecordgroup"
+  shared_record_group = nios_dns_sharedrecordgroup.parent_sharedrecordgroup.name
   target              = "server.example.com"
   weight              = 5
 }
@@ -13,7 +18,7 @@ resource "nios_dns_sharedrecord_srv" "sharedrecord_srv_additional_fields" {
   name                = "_http._tcp.example.com"
   port                = 80
   priority            = 20
-  shared_record_group = "example-sharedrecordgroup"
+  shared_record_group = nios_dns_sharedrecordgroup.parent_sharedrecordgroup.name
   target              = "webserver.example.com"
   weight              = 10
   comment             = "Example Shared SRV Record"
