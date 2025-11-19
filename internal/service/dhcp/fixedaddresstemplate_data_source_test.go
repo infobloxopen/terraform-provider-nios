d5f4bc47fac334ebd2927f92e73c5ca64c7ad232
VishrutiBuddhadev <vbuddhadev@infoblox.com>
2025-11-19 13:03:12 +0530

    fixed import ordering

--- internal/service/dhcp/fixedaddresstemplate_data_source_test.go
+++ internal/service/dhcp/fixedaddresstemplate_data_source_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
 	"github.com/infobloxopen/infoblox-nios-go-client/dhcp"
+
 	"github.com/infobloxopen/terraform-provider-nios/internal/acctest"
 )
 
