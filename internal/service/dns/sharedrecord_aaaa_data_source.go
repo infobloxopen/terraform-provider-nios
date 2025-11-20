3a32acc47184df828f24fe02ee617a30849438cb
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-11-20 14:57:10 +0530

    added description

--- internal/service/dns/sharedrecord_aaaa_data_source.go
+++ internal/service/dns/sharedrecord_aaaa_data_source.go
@@ -51,7 +51,7 @@ func (m *SharedrecordAaaaModelWithFilter) FlattenResults(ctx context.Context, fr
 
 func (d *SharedrecordAaaaDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		MarkdownDescription: "",
+		MarkdownDescription: "Retrieves information about existing DNS Shared AAAA Records.",
 		Attributes: map[string]schema.Attribute{
 			"filters": schema.MapAttribute{
 				Description: "Filter are used to return a more specific list of results. Filters can be used to match resources by specific attributes, e.g. name. If you specify multiple filters, the results returned will have only resources that match all the specified filters.",
