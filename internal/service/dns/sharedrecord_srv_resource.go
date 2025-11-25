b01d82e541bcae4c486c431d9a94f973649a71f8
Tejashree-RS <travisuryavamshi@infoblox.com>
2025-11-25 16:51:13 +0530

    modified import function

--- internal/service/dns/sharedrecord_srv_resource.go
+++ internal/service/dns/sharedrecord_srv_resource.go
@@ -220,5 +220,5 @@ func (r *SharedrecordSrvResource) Delete(ctx context.Context, req resource.Delet
 }
 
 func (r *SharedrecordSrvResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	resource.ImportStatePassthroughID(ctx, path.Root("ref"), req, resp)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("ref"), req.ID)...)
 }
