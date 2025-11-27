c616cbf28534b9f1faa60899289b7d77233f125c
Vishruti Buddhadev <vbuddhadev@infoblox.com>
2025-11-27 16:37:49 +0530

    [DHCP] Fixed Address Template Support For Terraform Provider (#261)
    * initial commit
    
    * fixed import ordering
    
    * addressed review comments
    
    * addressed review comments
    
    * added TODO
    
    * addressed review comments

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -144,6 +144,7 @@ func (p *NIOSProvider) Resources(_ context.Context) []func() resource.Resource {
 		dhcp.NewIpv6fixedaddresstemplateResource,
 		dhcp.NewDhcpoptionspaceResource,
 		dhcp.NewDhcpoptiondefinitionResource,
+		dhcp.NewFixedaddresstemplateResource,
 
 		dtc.NewDtcLbdnResource,
 		dtc.NewDtcServerResource,
@@ -242,6 +243,7 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 		dhcp.NewIpv6fixedaddresstemplateDataSource,
 		dhcp.NewDhcpoptionspaceDataSource,
 		dhcp.NewDhcpoptiondefinitionDataSource,
+		dhcp.NewFixedaddresstemplateDataSource,
 
 		dtc.NewDtcLbdnDataSource,
 		dtc.NewDtcServerDataSource,
