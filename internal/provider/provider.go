9b50ce83b240bb8e82c42240576188b9f0156cfe
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2025-11-27 10:13:22 +0530

    [IPAM] Resource and Data Source for VLAN Range (#272)
    * Resource and Data Source For VLAN View
    
    * Updated tests and other files
    
    * Updated Examples
    
    * misc changes
    
    * Updated Test name for VLAN Import
    
    * Addressed Review Comments
    
    * Updated Docs and added internal id
    
    * Added Changes for Import Functionality
    
    * Sorted Imports
    
    * Marked PreCreateVLAN and Vlan Name Prefix as Computed Attributes
    
    * Updated VLAN Name Prefix and Pre Create Vlan to use plan values
    
    * Added Validate Config for Vlan Name Prefix
    
    * Added necessary plan modifiers
    
    * Resource and Data Source for VLAN Range
    
    * Resource and Data Source Implementation for VLAN Range
    
    * Updated Go Client
    
    * Addressed Review Comments
    
    * Updated Planmodifiers
    
    * Resolved Conflicts and added Validate Config
    
    ---------
    
    Co-authored-by: Vishruti Buddhadev <vbuddhadev@infoblox.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -155,6 +155,7 @@ func (p *NIOSProvider) Resources(_ context.Context) []func() resource.Resource {
 		ipam.NewBulkhostnametemplateResource,
 		ipam.NewVlanviewResource,
 		ipam.NewVlanResource,
+		ipam.NewVlanrangeResource,
 
 		cloud.NewAwsrte53taskgroupResource,
 		cloud.NewAwsuserResource,
@@ -247,6 +248,7 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 		ipam.NewBulkhostnametemplateDataSource,
 		ipam.NewVlanviewDataSource,
 		ipam.NewVlanDataSource,
+		ipam.NewVlanrangeDataSource,
 
 		cloud.NewAwsrte53taskgroupDataSource,
 		cloud.NewAwsuserDataSource,
