36fcb27ea2e8ebddc33015ee58daeed9de4f6c39
Chaithra007 <chaithra@infoblox.com>
2025-11-27 09:45:59 +0530

    minor changes

--- internal/service/dtc/model_dtc_topology_rule_destination_link.go
+++ internal/service/dtc/model_dtc_topology_rule_destination_link.go
@@ -8,7 +8,6 @@ import (
 	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
-	// "github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 
 	"github.com/infobloxopen/infoblox-nios-go-client/dtc"
 
@@ -51,18 +50,6 @@ func ExpandDtcTopologyRuleDestinationLink(ctx context.Context, o types.String, d
 	}
 }
 
-// func (m *DtcTopologyRuleDestinationLinkModel) Expand(ctx context.Context, diags *diag.Diagnostics) *dtc.DtcTopologyRuleDestinationLink {
-// 	if m == nil {
-// 		return nil
-// 	}
-// 	to := &dtc.DtcTopologyRuleDestinationLink{
-// 		Ref:  flex.ExpandStringPointer(m.Ref),
-// 		Host: flex.ExpandStringPointer(m.Host),
-// 		Name: flex.ExpandStringPointer(m.Name),
-// 	}
-// 	return to
-// }
-
 func FlattenDtcTopologyRuleDestinationLink(ctx context.Context, from *dtc.DtcTopologyRuleDestinationLink, diags *diag.Diagnostics) types.String {
 	if from == nil {
 		return types.StringNull()
@@ -70,14 +57,3 @@ func FlattenDtcTopologyRuleDestinationLink(ctx context.Context, from *dtc.DtcTop
 	return flex.FlattenStringPointer(from.DtcTopologyRuleDestinationLinkOneOf.Ref)
 }
 
-// func (m *DtcTopologyRuleDestinationLinkModel) Flatten(ctx context.Context, from *dtc.DtcTopologyRuleDestinationLink, diags *diag.Diagnostics) {
-// 	if from == nil {
-// 		return
-// 	}
-// 	if m == nil {
-// 		*m = DtcTopologyRuleDestinationLinkModel{}
-// 	}
-// 	m.Ref = flex.FlattenStringPointer(from.Ref)
-// 	m.Host = flex.FlattenStringPointer(from.Host)
-// 	m.Name = flex.FlattenStringPointer(from.Name)
-// }
