23f47e116e8e0b68b9d44cc8e97f5409a9aa68c0
rsharmaInfoblox <rsharma6@infoblox.com>
2025-12-16 10:14:06 +0530

    NPA-1218: Added Resource and Datasource for RPZ AAAA record (#305)
    * NPA-1218: Added resource and datasource for rpz aaaa record
    
    * NPA-1218: added hcl configs and docs
    
    * NPA-1218: Added whitespace validation for name field
    
    ---------
    
    Co-authored-by: Vishruti Buddhadev <vbuddhadev@infoblox.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -212,6 +212,7 @@ func (p *NIOSProvider) Resources(_ context.Context) []func() resource.Resource {
 		rpz.NewRecordRpzMxResource,
 		rpz.NewRecordRpzSrvResource,
 		rpz.NewRecordRpzCnameResource,
+		rpz.NewRecordRpzAaaaResource,
 	}
 }
 
@@ -330,6 +331,7 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 		rpz.NewRecordRpzMxDataSource,
 		rpz.NewRecordRpzSrvDataSource,
 		rpz.NewRecordRpzCnameDataSource,
+		rpz.NewRecordRpzAaaaDataSource,
 	}
 }
 
