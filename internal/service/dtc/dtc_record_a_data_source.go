1e0aed0851755632b91adb5a7390174338af546c
Chaithra007 <chaithra@infoblox.com>
2026-01-05 11:34:01 +0530

    addressing PR review comments

--- internal/service/dtc/dtc_record_a_data_source.go
+++ internal/service/dtc/dtc_record_a_data_source.go
@@ -54,7 +54,7 @@ func (d *DtcRecordADataSource) Schema(ctx context.Context, req datasource.Schema
 		MarkdownDescription: "Retrieves information about existing DTC A Records.",
 		Attributes: map[string]schema.Attribute{
 			"filters": schema.MapAttribute{
-				Description: "Filter are used to return a more specific list of results. Filters can be used to match resources by specific attributes, e.g. name. If you specify multiple filters, the results returned will have only resources that match all the specified filters.The 'dtc_server' filter is required and must be specified for searching DTC A records.",
+				Description: "Filter are used to return a more specific list of results. Filters can be used to match resources by specific attributes, e.g. name. If you specify multiple filters, the results returned will have only resources that match all the specified filters. The `dtc_server` filter is a required and must be specified for searching DTC A records.",
 				ElementType: types.StringType,
 				Required:    true,
 				Validators: []validator.Map{
