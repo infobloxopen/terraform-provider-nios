8bdaa2eb06a818b3db57bbd0f329c53e46c48ce0
Rishabh Singh <126642800+rsingh4infoblox@users.noreply.github.com>
2026-03-10 15:27:13 +0530

    Apply suggestions from code review

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -226,7 +226,7 @@ func (p *NIOSProvider) Resources(_ context.Context) []func() resource.Resource {
 		misc.NewRulesetResource,
 		misc.NewBfdtemplateResource,
 		misc.NewDxlEndpointResource,
-    misc.NewSyslogEndpointResource,
+		misc.NewSyslogEndpointResource,
 
 		smartfolder.NewSmartfolderPersonalResource,
 		smartfolder.NewSmartfolderGlobalResource,
@@ -375,7 +375,7 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 		misc.NewRulesetDataSource,
 		misc.NewBfdtemplateDataSource,
 		misc.NewDxlEndpointDataSource,
-    misc.NewSyslogEndpointDataSource,
+		misc.NewSyslogEndpointDataSource,
 
 		smartfolder.NewSmartfolderPersonalDataSource,
 		smartfolder.NewSmartfolderGlobalDataSource,
