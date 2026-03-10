ef1f8f7e5cbf9d5dd0f396510be3e5b3b5fb0d95
rsharmaInfoblox <rsharma6@infoblox.com>
2026-03-10 15:16:27 +0530

    [RPZ][DNS][NPA-1590] Deprecate IsValidFQDN and migrate to IsValidDomainName validator (#399)
    * NPA-1590: Added changes to migrate all objs to new fqdn validator and deprecated the legacy one
    
    * NPA-1590: added fix for cname ipaddressdn resources found during ut
    
    * NPA-1590: incorporated review comments
    
    ---------
    
    Co-authored-by: Ujjwal Nasra <125353741+unasra@users.noreply.github.com>

--- internal/service/rpz/model_record_rpz_cname_ipaddressdn.go
+++ internal/service/rpz/model_record_rpz_cname_ipaddressdn.go
@@ -67,7 +67,7 @@ var RecordRpzCnameIpaddressdnResourceSchemaAttributes = map[string]schema.Attrib
 	"canonical": schema.StringAttribute{
 		Required: true,
 		Validators: []validator.String{
-			customvalidator.IsValidFQDN(),
+			customvalidator.IsValidDomainName(),
 		},
 		MarkdownDescription: "The canonical name in FQDN format. This value can be in unicode format.",
 	},
