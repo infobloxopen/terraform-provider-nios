83bf74a51a4dc00e1a5f8aaaa61839eea9de6a2a
rsharmaInfoblox <rsharma6@infoblox.com>
2026-03-11 11:40:50 +0530

    [MICROSOFT][NPA-1473] Added Resource and Datasource for MS Server (#395)
    * NPA-1473: Added initial changes for msserver obj
    
    * NPA-1473: added resource test cases for msserver
    
    * NPA-1473: fixed msserver test cases and added hcl examples and docs
    
    * NPA-1473: fixed password test case
    
    * NPA-1473: updated go mod with latest changes
    
    * NPA-1473: fixed docs and build failure issue
    
    * NPA-1473: Added validator for address field
    
    * NPA-1473: Assigned different IP to each test case for parallel execution
    
    ---------
    
    Co-authored-by: Ujjwal Nasra <125353741+unasra@users.noreply.github.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -26,6 +26,7 @@ import (
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/dtc"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/grid"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/ipam"
+	"github.com/infobloxopen/terraform-provider-nios/internal/service/microsoft"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/misc"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/notification"
 	"github.com/infobloxopen/terraform-provider-nios/internal/service/parentalcontrol"
@@ -264,6 +265,8 @@ func (p *NIOSProvider) Resources(_ context.Context) []func() resource.Resource {
 
 		parentalcontrol.NewParentalcontrolAvpResource,
 		parentalcontrol.NewParentalcontrolBlockingpolicyResource,
+
+		microsoft.NewMsserverResource,
 	}
 }
 
@@ -412,6 +415,8 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 
 		parentalcontrol.NewParentalcontrolAvpDataSource,
 		parentalcontrol.NewParentalcontrolBlockingpolicyDataSource,
+
+		microsoft.NewMsserverDataSource,
 	}
 }
 
