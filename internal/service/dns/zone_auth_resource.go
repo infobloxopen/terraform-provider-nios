a85a17af66a84c2cb38bfea24c51079e762a25b7
Aish-sp <163982471+Aish-sp@users.noreply.github.com>
2026-04-13 13:52:28 +0530

    [DNS][Fix][NPA-1694]: Enhancing validation for `ms_primaries` when `ms_sync_disabled` is set to true(#450)
    * Initial commit for sharedrecord:txt
    
    * Initial commit for sharedrecord:txt
    
    * Initial commit for sharedrecord:txt
    
    * Fix for NPA-1694
    
    * removing provider file

--- internal/service/dns/zone_auth_resource.go
+++ internal/service/dns/zone_auth_resource.go
@@ -82,8 +82,7 @@ func (r *ZoneAuthResource) ValidateConfig(ctx context.Context, req resource.Vali
 	}
 
 	if !data.MsSyncDisabled.IsNull() && !data.MsSyncDisabled.IsUnknown() && data.MsSyncDisabled.ValueBool() {
-		hasMsPrimaries := !data.MsPrimaries.IsNull() && !data.MsPrimaries.IsUnknown()
-		if !hasMsPrimaries {
+		if data.MsPrimaries.IsNull() && data.MsPrimaries.IsUnknown() {
 			resp.Diagnostics.AddError(
 				"Invalid Configuration",
 				"'ms_primaries' must be provided when 'ms_sync_disabled' is set.",
