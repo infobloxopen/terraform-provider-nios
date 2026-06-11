8ebdcf94e9f25946bf90c63464a3cc43391789a0
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2026-06-11 18:17:33 +0530

    Added List Suppport for DNS View

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -480,6 +480,7 @@ func (p *NIOSProvider) DataSources(ctx context.Context) []func() datasource.Data
 func (p *NIOSProvider) ListResources(ctx context.Context) []func() list.ListResource {
 	return []func() list.ListResource{
 		dns.NewRecordAList,
+		dns.NewViewList,
 		dns.NewZoneAuthList,
 		dns.NewZoneForwardList,
 		dns.NewZoneStubList,
