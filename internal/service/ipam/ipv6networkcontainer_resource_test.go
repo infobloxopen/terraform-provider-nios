ebf05c8f7e0f1a0d64d3dc741457d082bd2c1b40
hgupta-infoblox <hgupta1@infoblox.com>
2026-06-23 15:36:27 +0530

    added remaing todo

--- internal/service/ipam/ipv6networkcontainer_resource_test.go
+++ internal/service/ipam/ipv6networkcontainer_resource_test.go
@@ -19,6 +19,10 @@ import (
 
 // TODO: Members need a valid member to test members and GO client does not support it yet.
 // TODO: send rir request need rir organization configuration required
+// TODO: Federated realms serve need to enabled.
+// TODO: ZoneAssociations need dns zone to test associations.
+// TODO: SubscribeSettings need a valid subscribe settings to test subscribe settings.
+
 var readableAttributesForIpv6networkcontainer = "cloud_info,comment,ddns_domainname,ddns_enable_option_fqdn,ddns_generate_hostname,ddns_server_always_updates,ddns_ttl,discover_now_status,discovery_basic_poll_settings,discovery_blackout_setting,discovery_engine_type,discovery_member,domain_name_servers,enable_ddns,enable_discovery,endpoint_sources,extattrs,last_rir_registration_update_sent,last_rir_registration_update_status,logic_filter_rules,mgm_private,mgm_private_overridable,ms_ad_user_data,network,network_container,network_view,options,port_control_blackout_setting,preferred_lifetime,rir,rir_organization,rir_registration_status,same_port_control_discovery_blackout,subscribe_settings,unmanaged,update_dns_on_lease_renewal,use_blackout_setting,use_ddns_domainname,use_ddns_enable_option_fqdn,use_ddns_generate_hostname,use_ddns_ttl,use_discovery_basic_polling_settings,use_domain_name_servers,use_enable_ddns,use_enable_discovery,use_logic_filter_rules,use_mgm_private,use_options,use_preferred_lifetime,use_subscribe_settings,use_update_dns_on_lease_renewal,use_valid_lifetime,use_zone_associations,utilization,valid_lifetime,zone_associations"
 
 func TestAccIpv6networkcontainerResource_basic(t *testing.T) {
@@ -2222,20 +2226,6 @@ resource "nios_ipam_ipv6network_container" "test_enable_immediate_discovery" {
     use_enable_discovery = true
 }
 `, network, discoveryMember, enableImmediateDiscovery)
-}
-
-func testAccIpv6networkcontainerFederatedRealms(network, realmName, realmId string) string {
-	return fmt.Sprintf(`
-resource "nios_ipam_ipv6network_container" "test_federated_realms" {
-    network = %q
-    federated_realms = [
-      {
-        name = %q
-        id   = %q
-      }
-    ]
-}
-`, network, realmName, realmId)
 }
 
 func testAccIpv6networkcontainerLogicFilterRules(network string, logicFilterRules []map[string]any) string {
