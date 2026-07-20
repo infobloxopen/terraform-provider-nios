4ffd92d9c1e9766928d9e5beff1bf5e22bfa869e
Chaithra007 <chaithra@infoblox.com>
2026-07-21 00:14:52 +0530

    added basetypes validator

--- internal/service/ipam/networkcontainer_resource.go
+++ internal/service/ipam/networkcontainer_resource.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 
 	niosclient "github.com/infobloxopen/infoblox-nios-go-client/client"
 
